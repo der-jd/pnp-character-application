6918a7a02c5ae7de28da8ebfad78293a6fc083ea
Julian <89513886+der-jd@users.noreply.github.com>
2024-10-20 21:43:31 +0200

    Remove optional and deprecated arguments

--- terraform/frontend-distribution.tf
+++ terraform/frontend-distribution.tf
@@ -16,22 +16,12 @@ resource "aws_cloudfront_distribution" "frontend_distribution" {
   default_root_object = "index.html"
 
   default_cache_behavior {
+    cache_policy_id = "pnp-application-frontend-caching-policy"
     allowed_methods        = ["GET", "HEAD"]
     cached_methods         = ["GET", "HEAD"]
     target_origin_id       = "S3-${aws_s3_bucket.frontend_bucket.id}"
     viewer_protocol_policy = "redirect-to-https"
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl     = 0
     default_ttl = 3600
-    max_ttl     = 86400
   }
 
   price_class = "PriceClass_100"
