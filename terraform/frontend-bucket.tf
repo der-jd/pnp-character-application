3c32b8e03159afc888fb5118cc0304000767ac8d
Julian <89513886+der-jd@users.noreply.github.com>
2024-10-20 22:57:18 +0200

    Use s3 bucket REST endpoint to expose website via CloudFlare
    On this way the bucket content must not be directly public (as it would be if we use static website hosting directly in s3)

--- terraform/frontend-bucket.tf
+++ terraform/frontend-bucket.tf
@@ -5,18 +5,6 @@ resource "aws_s3_bucket" "frontend_bucket" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_website_configuration" "static_website" {
-  bucket = aws_s3_bucket.frontend_bucket.bucket
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
-
 resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
   bucket = aws_s3_bucket.frontend_bucket.id
 
@@ -24,10 +12,12 @@ resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Effect    = "Allow"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.frontend_bucket.arn}/*"
-        Principal = "*"
+        Effect   = "Allow"
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.frontend_bucket.arn}/*"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
         Condition = {
           StringEquals = {
             "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.frontend_distribution.id}"
@@ -48,8 +38,4 @@ resource "aws_s3_object" "error_html" {
   bucket = aws_s3_bucket.frontend_bucket.bucket
   key    = "error.html"
   source = "../frontend/error.html"
-}
-
-output "frontend_bucket_domain_name" {
-  value = aws_s3_bucket_website_configuration.static_website.website_endpoint
 }
--- terraform/frontend-distribution.tf
+++ terraform/frontend-distribution.tf
@@ -1,16 +1,11 @@
 // https://repost.aws/knowledge-center/cloudfront-serve-static-website
+// https://docs.aws.amazon.com/AmazonS3/latest/userguide/WebsiteEndpoints.html#WebsiteRestEndpointDiff
 
 resource "aws_cloudfront_distribution" "frontend_distribution" {
   origin {
-    domain_name = aws_s3_bucket_website_configuration.static_website.website_endpoint
-    origin_id   = "S3-${aws_s3_bucket.frontend_bucket.id}"
-
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1.2"]
-    }
+    domain_name              = aws_s3_bucket_website_configuration.static_website.website_endpoint
+    origin_id                = "S3-${aws_s3_bucket.frontend_bucket.id}"
+    origin_access_control_id = aws_cloudfront_origin_access_control.frontend_oac
   }
 
   enabled             = true
@@ -36,6 +31,14 @@ resource "aws_cloudfront_distribution" "frontend_distribution" {
   viewer_certificate {
     cloudfront_default_certificate = true
   }
+}
+
+resource "aws_cloudfront_origin_access_control" "frontend_oac" {
+  name                              = "frontend-oac"
+  description                       = "Origin Access Control for accessing S3 bucket securely"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
 }
 
 output "cloudfront_domain_name" {
