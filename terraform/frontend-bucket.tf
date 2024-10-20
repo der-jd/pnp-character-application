fa27fc2f6a17baf642ed57d9e4a8d6890080e0d8
Julian <89513886+der-jd@users.noreply.github.com>
2024-10-20 23:17:19 +0200

    Add docu link

--- terraform/frontend-bucket.tf
+++ terraform/frontend-bucket.tf
@@ -8,6 +8,7 @@ resource "aws_s3_bucket" "frontend_bucket" {
 resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
   bucket = aws_s3_bucket.frontend_bucket.id
 
+  // https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/private-content-restricting-access-to-s3.html
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
