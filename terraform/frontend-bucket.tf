5edca1d611c1c479b5423569ebd5e899fd48534c
Julian <89513886+der-jd@users.noreply.github.com>
2024-10-20 13:08:39 +0200

    Add CloudFront

--- terraform/frontend-bucket.tf
+++ terraform/frontend-bucket.tf
@@ -18,6 +18,28 @@ resource "aws_s3_bucket_website_configuration" "static_website" {
   }
 }
 
+resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = "s3:GetObject"
+        Effect   = "Allow"
+        Resource = "${aws_s3_bucket.frontend_bucket.arn}/*"
+        Principal = {
+          AWS = aws_cloudfront_origin_access_identity.oai.iam_arn
+        }
+      }
+    ]
+  })
+}
+
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.frontend_distribution.domain_name
+}
+
 output "frontend_bucket_domain_name" {
   value = aws_s3_bucket_website_configuration.static_website.website_endpoint
 }
---
+++ terraform/frontend-distribution.tf
@@ -0,0 +1,50 @@
+resource "aws_cloudfront_distribution" "frontend_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.frontend_bucket.bucket_regional_domain_name
+    origin_id   = "S3-${aws_s3_bucket.frontend_bucket.id}"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "S3-${aws_s3_bucket.frontend_bucket.id}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+# CloudFront Origin Access Identity (OAI) to restrict S3 bucket access to CloudFront
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "OAI for frontend S3 bucket"
+}
