b42c7e51c1ff1763b07cc8f3d84960350a36b9e1
Julian <89513886+der-jd@users.noreply.github.com>
2024-10-25 00:55:48 +0200

    Update file location

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -4,7 +4,7 @@ resource "aws_lambda_function" "increase_skill_lambda" {
   runtime       = "nodejs20.x"
 
   role     = aws_iam_role.lambda_exec_role.arn
-  filename = "../backend/skill_calculation.zip"
+  filename = "../backend/dist/skill-calculation.zip"
 }
 
 resource "aws_iam_role" "lambda_exec_role" {
