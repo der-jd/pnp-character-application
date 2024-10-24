52c7c11caf6c0e30046aff382119970e1b1348ee
Julian <89513886+der-jd@users.noreply.github.com>
2024-10-25 01:47:47 +0200

    fix script

--- backend/zip-all-lambdas.sh
+++ backend/zip-all-lambdas.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
 
 lambdas=("another-lambda" "skill-calculation")
 
+mkdir --parents dist
 for lambda in "${lambdas[@]}"
 do
   echo "Zipping $lambda..."
