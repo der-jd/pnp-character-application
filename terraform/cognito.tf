222170cd76c7d420c66be38d70ff3382d0fe38d7
Philipp Eich <philipp.eich1992@gmail.com>
2024-10-26 12:45:51 +0200

    added validity units

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -24,6 +24,12 @@ resource "aws_cognito_user_pool_client" "pnp_user_pool_client" {
   id_token_validity                    = 12
   refresh_token_validity               = 1
 
+  token_validity_units {
+    access_token  = "hours"
+    id_token      = "hours"
+    refresh_token = "days"
+  }
+
 }
 
 resource "aws_cognito_identity_pool" "pnp_identity_pool" {
