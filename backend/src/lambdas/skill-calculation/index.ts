e6e515b4ebf19bc1196ab166e0ecdd8f267c44a9
Julian <89513886+der-jd@users.noreply.github.com>
2024-10-26 20:46:10 +0200

    fix: Use async function
    Otherwise Lambda returns null

--- backend/src/lambdas/skill-calculation/index.ts
+++ backend/src/lambdas/skill-calculation/index.ts
@@ -1,6 +1,6 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
-export const handler = (event: APIGatewayProxyEvent): APIGatewayProxyResult => {
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   return calcSkillValue(event);
 };
 
