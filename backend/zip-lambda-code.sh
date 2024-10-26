72740794c0204b989c354fd05b2a983e740bed25
Julian <89513886+der-jd@users.noreply.github.com>
2024-10-26 21:31:03 +0200

    Update comments

--- backend/zip-lambda-code.sh
+++ backend/zip-lambda-code.sh
@@ -3,13 +3,17 @@
 # Exit immediately if a command exits with a non-zero status (relevant for the "for"-loop)
 set -e
 
+build_dir="build"
+dist_dir="dist"
 lambdas=("another-lambda" "skill-calculation")
 
-mkdir --parents dist
+mkdir --parents $dist_dir
+
+echo "Package Lambda code in '$build_dir' directory"
 for lambda in "${lambdas[@]}"
 do
   echo "Zipping code for $lambda..."
-  zip -r -j dist/$lambda.zip build/$lambda/*
+  zip -r -j $dist_dir/$lambda.zip $build_dir/$lambda/*
 done
 
-echo "Code for all Lambda functions zipped!"
+echo "Zip files created and saved in '$dist_dir'!"
