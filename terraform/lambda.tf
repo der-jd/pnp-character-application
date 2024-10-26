68d58b737d4eef9c4703913c2f26cfdd771fa7b8
Julian <89513886+der-jd@users.noreply.github.com>
2024-10-26 21:50:48 +0200

    Add todo comment

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -4,7 +4,7 @@ resource "aws_lambda_function" "increase_skill_lambda" {
   runtime       = "nodejs20.x"
 
   role     = aws_iam_role.lambda_exec_role.arn
-  filename = "../backend/dist/skill-calculation.zip"
+  filename = "../backend/dist/skill-calculation.zip" // TODO code changes are not automatically applied with terraform apply --> use zip file name based on git hash?!
 }
 
 resource "aws_iam_role" "lambda_exec_role" {
