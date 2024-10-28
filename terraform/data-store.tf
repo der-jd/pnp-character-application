3c31bcf2f53ac7f533872507d27bb849cbd77ea1
Julian <89513886+der-jd@users.noreply.github.com>
2024-10-28 18:45:56 +0100

    Add DynamoDB tables (#9)

---
+++ terraform/data-store.tf
@@ -0,0 +1,32 @@
+resource "aws_dynamodb_table" "characters" {
+  name         = "pnp-app-characters"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "character-id"
+
+  attribute {
+    name = "character-id"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "characters_history" {
+  name         = "pnp-app-characters-history"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "history-id"
+
+  attribute {
+    name = "history-id"
+    type = "S"
+  }
+
+  attribute {
+    name = "character-id"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "character-id"
+    hash_key        = "character-id"
+    projection_type = "ALL"
+  }
+}
