45dcb03dbacc449027cc54481016e63d5f931fe8
Philipp Eich <philipp.eich1992@gmail.com>
2024-11-01 09:09:13 +0100

    s3 bucket deploying setup

--- frontend/src/app/protected/dashboard/page.tsx
+++ frontend/src/app/protected/dashboard/page.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import { withAuth } from '../../components/withAuth'
-
 function Dashboard() {
   return (
     <div className="container mx-auto px-4 py-8">
--- terraform/frontend-bucket.tf
+++ terraform/frontend-bucket.tf
@@ -5,6 +5,33 @@ resource "aws_s3_bucket" "frontend_bucket" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket_ownership_controls" "frontend_bucket_ownership_controls" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend_bucket_public_access_block" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "frontend_bucket_acl" {
+
+  depends_on = [
+    aws_s3_bucket_ownership_controls.frontend_bucket_ownership_controls,
+    aws_s3_bucket_public_access_block.frontend_bucket_public_access_block
+  ]
+
+  bucket = aws_s3_bucket.frontend_bucket.id
+  acl = "public-read"
+}
+
 resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
   bucket = aws_s3_bucket.frontend_bucket.id
 
@@ -13,6 +40,7 @@ resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
     Version = "2012-10-17"
     Statement = [
       {
+        Sid = "PublicReadGedObject"
         Effect   = "Allow"
         Action   = "s3:GetObject"
         Resource = "${aws_s3_bucket.frontend_bucket.arn}/*"
@@ -27,4 +55,13 @@ resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
       }
     ]
   })
+}
+
+resource "aws_s3_object" "upload_object" {
+  for_each = fileset("frontend/build/", "*")
+  bucket = aws_s3_bucket.frontend_bucket.id
+  key = each.value
+  source = "frontend/build/${each.value}"
+  etag = filemd5("frontend/build/${each.value}")
+  content_type = "html/javascript"
 }
