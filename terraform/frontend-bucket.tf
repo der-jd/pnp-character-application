96cca30273a8e3c9c3e1cc0a867086a8711593b4
Philipp Eich <philipp.eich1992@gmail.com>
2024-11-01 10:32:40 +0100

    format

--- terraform/frontend-bucket.tf
+++ terraform/frontend-bucket.tf
@@ -5,32 +5,7 @@ resource "aws_s3_bucket" "frontend_bucket" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_ownership_controls" "frontend_bucket_ownership_controls" {
-  bucket = aws_s3_bucket.frontend_bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
 
-resource "aws_s3_bucket_public_access_block" "frontend_bucket_public_access_block" {
-  bucket = aws_s3_bucket.frontend_bucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_acl" "frontend_bucket_acl" {
-
-  depends_on = [
-    aws_s3_bucket_ownership_controls.frontend_bucket_ownership_controls,
-    aws_s3_bucket_public_access_block.frontend_bucket_public_access_block
-  ]
-
-  bucket = aws_s3_bucket.frontend_bucket.id
-  acl    = "public-read"
-}
 
 resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
   bucket = aws_s3_bucket.frontend_bucket.id
@@ -50,7 +25,6 @@ resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
         Condition = {
           StringEquals = {
             "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.frontend_distribution.id}",
-            "s3:ExistingObjectTag/public" : "yes"
           }
         }
       }
