0fc71fce8eb9f5485e446a1b1d6d68c2435a633a
Julian <89513886+der-jd@users.noreply.github.com>
2024-11-02 12:27:57 +0100

    Update eslint config

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -3,15 +3,18 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default [
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ["**/*.{js,mjs,cjs,ts}"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
   },
   {
-    ignores: ["node_modules/", "build/", "dist/"],
+    ignores: ["**/node_modules/", "build/", "dist/"],
   },
   {
     languageOptions: { globals: globals.node },
   },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
 ];
