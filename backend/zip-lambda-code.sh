0ac0409f8ee8c8a51cf1b5e5bea1c15dad1a239f
Julian <89513886+der-jd@users.noreply.github.com>
2024-11-10 21:44:18 +0100

    fix path for zipping

--- backend/zip-lambda-code.sh
+++ backend/zip-lambda-code.sh
@@ -3,7 +3,7 @@
 # Exit immediately if a command exits with a non-zero status (relevant for the "for"-loop)
 set -e
 
-build_dir="build"
+build_dir="build/lambdas"
 dist_dir="dist"
 
 # Gather the names of all subdirectories (Lambda functions) in the build_dir
@@ -18,7 +18,7 @@ echo "Package Lambda code in '$build_dir' directory"
 for lambda in "${lambdas[@]}"
 do
   echo "Zipping code for $lambda..."
-  zip -r -j $dist_dir/$lambda.zip $build_dir/$lambda/*
+  zip --recurse-paths --junk-paths $dist_dir/$lambda.zip $build_dir/$lambda/*
 done
 
 echo "Zip files created and saved in '$dist_dir'!"
