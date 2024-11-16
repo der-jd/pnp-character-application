0cf0bfb3e31a57d8420dd92f1c67ccbf3a8bc5df
Julian <89513886+der-jd@users.noreply.github.com>
2024-11-16 18:48:10 +0100

    fix: correct patg

--- backend/include-dependencies.sh
+++ backend/include-dependencies.sh
@@ -3,26 +3,27 @@
 set -e # Exit immediately if any command exits with a non-zero status. Needed for the for loop below
 
 start_dir=$PWD
-build_dir="build/lambdas"
+build_dir="build"
+lambda_build_dir="$build_dir/lambdas"
 src_dir="src/lambdas"
 
-# Gather the names of all subdirectories (Lambda functions) in the build_dir
+# Gather the names of all subdirectories (Lambda functions) in the lambda_build_dir
 lambdas=()
 while IFS= read -r -d '' dir; do
   lambdas+=("$(basename "$dir")")
-done < <(find "$build_dir" -mindepth 1 -maxdepth 1 -type d -print0)
+done < <(find "$lambda_build_dir" -mindepth 1 -maxdepth 1 -type d -print0)
 
 echo "Include dependencies in Lambda code"
 for lambda in "${lambdas[@]}"
 do
   echo "Copying relative dependencies..."
-  cp --verbose $build_dir/*.js $build_dir/$lambda
+  cp --verbose $build_dir/*.js $lambda_build_dir/$lambda
 
   echo "Copying package*.json files..."
-  cp --verbose $src_dir/$lambda/package*.json $build_dir/$lambda
+  cp --verbose $src_dir/$lambda/package*.json $lambda_build_dir/$lambda
 
-  echo "Installing prod dependencies in $build_dir/$lambda..."
-  cd $build_dir/$lambda
+  echo "Installing prod dependencies in $lambda_build_dir/$lambda..."
+  cd $lambda_build_dir/$lambda
   npm install --omit=dev
 
   cd $start_dir
