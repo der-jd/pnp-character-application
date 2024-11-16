43d064bf349f63b0f2417ae1e2aef85fe2b0bdf9
Julian <89513886+der-jd@users.noreply.github.com>
2024-11-16 17:07:43 +0100

    fix: add version constraint

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5"
     }
+
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2"
+    }
   }
 
   required_version = ">= 1.9.8"
