b71fb4a30d62e2b7acabc5fca506eb1faea8fda6
Julian <89513886+der-jd@users.noreply.github.com>
2024-11-19 00:57:27 +0100

    Use sub dir for lambda layer

--- backend/include-dependencies.sh
+++ backend/include-dependencies.sh
@@ -3,23 +3,49 @@
 set -e # Exit immediately if any command exits with a non-zero status. Needed for the for loop below
 
 start_dir=$PWD
-lambda_build_dir="build/lambdas"
-src_dir="src/lambdas"
+lambdas_build_dir="build/lambdas"
+lambda_layers_build_dir="build/lambda-layers"
+src_dir="src"
 
-# Gather the names of all subdirectories (Lambda functions) in the lambda_build_dir
+# Gather the names of all subdirectories (Lambda functions) in the lambdas_build_dir
 lambdas=()
 while IFS= read -r -d '' dir; do
   lambdas+=("$(basename "$dir")")
-done < <(find "$lambda_build_dir" -mindepth 1 -maxdepth 1 -type d -print0)
+done < <(find "$lambdas_build_dir" -mindepth 1 -maxdepth 1 -type d -print0)
 
-echo "Include dependencies in Lambda code"
+echo "Include dependencies for Lambda functions"
 for lambda in "${lambdas[@]}"
 do
   echo "Copying package*.json files..."
-  cp --verbose $src_dir/$lambda/package*.json $lambda_build_dir/$lambda
+  lambda_dest_dir="$lambdas_build_dir/$lambda"
+  cp --verbose $src_dir/lambdas/$lambda/package*.json $lambda_dest_dir
 
-  echo "Installing prod dependencies in $lambda_build_dir/$lambda..."
-  cd $lambda_build_dir/$lambda
+  echo "Installing prod dependencies in $lambda_dest_dir..."
+  cd $lambda_dest_dir
+  npm install --omit=dev
+
+  cd $start_dir
+done
+
+# Gather the names of all subdirectories (Lambda Layers) in the lambda_layers_build_dir
+lambda_layers=()
+while IFS= read -r -d '' dir; do
+  lambda_layers+=("$(basename "$dir")")
+done < <(find "$lambda_layers_build_dir" -mindepth 1 -maxdepth 1 -type d -print0)
+
+# https://docs.aws.amazon.com/lambda/latest/dg/nodejs-layers.html
+echo "Include dependencies for Lambda Layers"
+for layer in "${lambda_layers[@]}"
+do
+  layer_dest_dir="$lambda_layers_build_dir/$layer/nodejs"
+  mkdir --parent $layer_dest_dir
+
+  # TODO copy package files to correct dir (nodejs) and then run npm install or run it at first and after that copy the output (node_modules) to the nodejs dir?!
+  echo "Copying package*.json files..."
+  cp --verbose $src_dir/lambda-layers/$layer/package*.json $layer_dest_dir
+
+  echo "Installing prod dependencies in $layer_dest_dir..."
+  cd $layer_dest_dir
   npm install --omit=dev
 
   cd $start_dir
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,12 +1,12 @@
 data "archive_file" "configuration" {
   type        = "zip"
-  source_dir  = "../backend/build/lambda-layer"
-  output_path = "../backend/dist/lambda-layer.zip"
+  source_dir  = "../backend/build/lambda-layers/configuration"
+  output_path = "../backend/dist/configuration.zip"
 }
 
 resource "aws_lambda_layer_version" "configuration" {
   layer_name          = "configuration"
-  filename            = "../backend/dist/lambda-layer.zip"
+  filename            = "../backend/dist/configuration.zip"
   source_code_hash    = data.archive_file.configuration.output_base64sha256
   compatible_runtimes = ["nodejs20.x"]
 }
