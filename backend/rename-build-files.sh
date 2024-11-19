e6b147e71793466b39bd7deaa99c07f8107a816f
Julian <89513886+der-jd@users.noreply.github.com>
2024-11-19 19:47:27 +0100

    Remove obsolete mkdir command

--- backend/rename-build-files.sh
+++ backend/rename-build-files.sh
@@ -2,8 +2,6 @@
 
 build_dir="build"
 
-mkdir --parents $build_dir
-
 echo "Rename all .js files in '$build_dir' to .mjs"
 find "$build_dir" -type f -name "*.js" | while read -r file; do
   new_file="${file%.js}.mjs"
