31c139bea8ede478445417686434c772474197f1
Julian <89513886+der-jd@users.noreply.github.com>
2024-11-19 21:49:02 +0100

    fix: The index file for the config package (Lambda Layer) must end on .js

--- backend/rename-build-files.sh
+++ backend/rename-build-files.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-build_dir="build"
+build_dir="build/lambdas"
 
-echo "Rename all .js files in '$build_dir' to .mjs"
+echo "Rename all .js files in '$build_dir' to .mjs so that AWS Lambda recognizes them as ES Modules"
 find "$build_dir" -type f -name "*.js" | while read -r file; do
   new_file="${file%.js}.mjs"
   echo "Renaming $file -> $new_file"
