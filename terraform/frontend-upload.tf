66c088706a2e05670980cb3b7b77fe41d3cdb1be
Philipp Eich <philipp.eich1992@gmail.com>
2024-11-24 12:02:21 +0100

    fixed typo

--- terraform/frontend-upload.tf
+++ terraform/frontend-upload.tf
@@ -1,12 +1,12 @@
 locals {
-  frontend_path = "../frontend/build"
+  frontend_path = "../frontend/build/"
 }
 
 resource "aws_s3_object" "static_site_upload_object" {
-  for_each     = fileset(local.frontend_path, "**/*")
+  for_each     = fileset(local.frontend_path, "**/*.*")
   bucket       = aws_s3_bucket.frontend_bucket.id
   key          = each.value
-  source       = "${local.frontend_path} / ${each.value}"
-  etag         = filemd5("${local.frontend_path}/${each.value}")
-  content_type = "text/html"
+  source       = "${local.frontend_path}${each.value}"
+  etag         = filemd5("${local.frontend_path}${each.value}")
+  acl          = "public-read"
 }
