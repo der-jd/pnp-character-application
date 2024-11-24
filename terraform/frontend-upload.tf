b85a07010cf90832c9eac9a9814476e5440c2a80
Philipp Eich <philipp.eich1992@gmail.com>
2024-11-24 16:17:41 +0100

    changed wildcard operator

--- terraform/frontend-upload.tf
+++ terraform/frontend-upload.tf
@@ -3,10 +3,9 @@ locals {
 }
 
 resource "aws_s3_object" "static_site_upload_object" {
-  for_each = fileset(local.frontend_path, "**")
+  for_each = fileset(local.frontend_path, "**/*.*")
   bucket   = aws_s3_bucket.frontend_bucket.id
   key      = each.value
   source   = "${local.frontend_path}${each.value}"
   etag     = filemd5("${local.frontend_path}${each.value}")
-  acl      = "public-read"
 }
