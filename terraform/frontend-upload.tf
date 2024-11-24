30f16d5c6d2d71131c53ed3956d4256fd9e6cc2d
Philipp Eich <philipp.eich1992@gmail.com>
2024-11-24 11:10:59 +0100

    formating tf code

--- terraform/frontend-upload.tf
+++ terraform/frontend-upload.tf
@@ -6,7 +6,7 @@ resource "aws_s3_object" "static_site_upload_objecte" {
   for_each     = fileset(local.frontend_path, "**")
   bucket       = aws_s3_bucket.frontend_bucket.id
   key          = each.value
-  source       = local.frontend_path / each.value
-  etag         = filemd5(local.frontend_path / each.value)
+  source       = "${local.frontend_path} / ${each.value}"
+  etag         = filemd5("${local.frontend_path}/${each.value}")
   content_type = "text/html"
 }
