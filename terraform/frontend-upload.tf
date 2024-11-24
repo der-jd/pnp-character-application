e9020e0dabb5157b05c4aa21c1f55d1ed1840188
Philipp Eich <philipp.eich1992@gmail.com>
2024-11-24 10:53:14 +0100

    corrected naming

--- terraform/frontend-upload.tf
+++ terraform/frontend-upload.tf
@@ -1,12 +1,12 @@
-local {
+locals {
   frontend_path = "../fronend/build"
 }
 
 resource "aws_s3_object" "static_site_upload_objecte" {
-  for_each     = fileset(local.frontend_path, "**/*")
+  for_each     = fileset(locals.frontend_path, "**/*")
   bucket       = aws_s3_bucket.frontend_bucket.id
   key          = each.value
-  source       = local.frontend_path / each.value
-  etag         = filemd5(local.frontend_path / each.value)
+  source       = locals.frontend_path / each.value
+  etag         = filemd5(locals.frontend_path / each.value)
   content_type = "text/html"
 }
