3064ddacbb6b615bdbea5f12ee8f8542c2b80138
Philipp Eich <philipp.eich1992@gmail.com>
2024-11-30 12:54:07 +0100

    added unified build id, added upload script for frontend to terraform

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    experimental: {
-        missingSuspenseWithCSRBailout: false,
-      },
-    images: {
+  output: 'export',
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+  images: {
     unoptimized: true,
-    },
+  },
 };
 
-export default nextConfig;
+export default {
+  ...nextConfig,
+  generateBuildId: async () => {
+    // This could be anything, using the latest git hash
+    return '42'; // Use a string for safety
+  },
+};
