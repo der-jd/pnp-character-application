1bec48ce2b7444fd805a059170f1ecb1238b6fee
Julian <89513886+der-jd@users.noreply.github.com>
2024-12-01 12:22:59 +0100

    Update SID name

--- terraform/frontend-bucket.tf
+++ terraform/frontend-bucket.tf
@@ -15,7 +15,7 @@ resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid      = "PublicReadGedObject"
+        Sid      = "ReadAccessForCloudFront"
         Effect   = "Allow"
         Action   = "s3:GetObject"
         Resource = "${aws_s3_bucket.frontend_bucket.arn}/*"
