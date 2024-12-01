37ca2d63b235a52f9f17a215a40fc6702ee70b56
Julian <89513886+der-jd@users.noreply.github.com>
2024-12-01 12:49:31 +0100

    Add Lambda function to get character from DynamoDB (#16)
    - Add Lambda function to get character as JSON object from DynamoDB
    - Restructure Terraform code & files for Lambdas: deploy multiple
    lambdas
    - fix: Explicitly block public access for the frontend S3 bucket in
    Terraform

--- terraform/frontend-bucket.tf
+++ terraform/frontend-bucket.tf
@@ -5,7 +5,13 @@ resource "aws_s3_bucket" "frontend_bucket" {
   force_destroy = true
 }
 
-
+resource "aws_s3_bucket_public_access_block" "frontend_bucket_access" {
+  bucket                  = aws_s3_bucket.frontend_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
 
 resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
   bucket = aws_s3_bucket.frontend_bucket.id
@@ -15,7 +21,7 @@ resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid      = "PublicReadGedObject"
+        Sid      = "ReadAccessForCloudFront"
         Effect   = "Allow"
         Action   = "s3:GetObject"
         Resource = "${aws_s3_bucket.frontend_bucket.arn}/*"
