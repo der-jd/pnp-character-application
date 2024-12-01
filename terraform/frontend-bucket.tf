1d62c4e68667235e024b87da6b64e793cf84e661
Julian <89513886+der-jd@users.noreply.github.com>
2024-12-01 12:26:54 +0100

    Add block public access for frontend bucket

--- terraform/frontend-bucket.tf
+++ terraform/frontend-bucket.tf
@@ -5,6 +5,14 @@ resource "aws_s3_bucket" "frontend_bucket" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket_public_access_block" "frontend_bucket_access" {
+  bucket                  = aws_s3_bucket.frontend_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
   bucket = aws_s3_bucket.frontend_bucket.id
 
