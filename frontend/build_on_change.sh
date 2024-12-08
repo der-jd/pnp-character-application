c5bfaa21d3a09566df5630827b779b86ff492054
Philipp Eich <philipp.eich1992@gmail.com>
2024-12-08 09:59:42 +0100

    changed build script to check for empty bucket, moved upload to script

--- frontend/build_on_change.sh
+++ frontend/build_on_change.sh
@@ -24,16 +24,34 @@ if [ ! -f "$CACHED_CHECKSUM" ]; then
 else
     diff $CACHED_CHECKSUM current_checksum.txt > /dev/null
     if [ $? -eq 0 ]; then
-        echo "No changes detected in frontend folder, skipping build!"
-        exit 0
+        echo "No changes detected in frontend folder, checking bucket content!"
+        
+        # The bucket should be at least accessible
+        if ! aws s3 ls s3://pnp-character-application-frontend --region eu-central-1 > /dev/null 2>&1; then
+            echo "Bucket does not exist or access is restricted, please check terraform setup!"
+            echo "Skipping build since the upload destination is inaccessible"
+            exit 2
+        fi
+        
+        BUCKET_EMPTY=$(aws s3 ls s3://pnp-character-application-frontend --region eu-central-1 --recursive)
+
+        # Bucket should be empty if recreated, in this case we want to build and deploy
+        if [ -n "$BUCKET_EMPTY" ]; then
+            echo "Bucket populated, skipping build!"
+            exit 0
+        fi
+        
+        echo "Bucket is empty! Regenerating build files"
     else
-        echo "Detected changes in frontend folder! regenerating build files!"
+        echo "Detected changes in frontend folder! Regenerating build files!"
     fi
 fi
 
 rm -rf build
 npm run build
 mv out build
+
+aws s3 sync build s3://pnp-character-application-frontend --region eu-central-1
 
 rm -f checksum.txt
 mv current_checksum.txt checksum.txt
