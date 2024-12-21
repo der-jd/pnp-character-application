de3e52830ac3ad36f6ce5662cd1ef27075632db2
Philipp Eich <philipp.eich1992@gmail.com>
2024-12-21 19:29:54 +0100

    fixed errors

--- backend/src/lambdas/create-tenant-id/index.ts
+++ backend/src/lambdas/create-tenant-id/index.ts
@@ -1,91 +1,98 @@
-import * as AWS from 'aws-sdk';
-import * as jwt from 'jsonwebtoken';
+import * as AWS from "aws-sdk";
+import * as jwt from "jsonwebtoken";
 
 const cognito = new AWS.CognitoIdentityServiceProvider();
 
 export const handler = async (event: any): Promise<any> => {
   const userPoolId = process.env.USER_POOL_ID;
-  const clientId = process.env.CLIENT_ID; 
+  const clientId = process.env.CLIENT_ID;
 
   if (!userPoolId || !clientId) {
     return {
       statusCode: 500,
       body: JSON.stringify({
-        message: 'User pool ID or client ID is not set in environment variables.',
+        message: "User pool ID or client ID is not set in environment variables.",
       }),
     };
   }
-  
-  const token = event.headers.Authorization.split(' ')[1]; 
+
+  const token = event.headers.Authorization.split(" ")[1];
 
   let sub: string;
   try {
     const decoded: any = jwt.decode(token);
     if (!decoded.sub) {
-      throw new Error('Token does not contain sub');
+      throw new Error("Token does not contain sub");
     }
     sub = decoded.sub;
   } catch (error) {
-    console.error('Error decoding token:', error);
+    console.error("Error decoding token:", error);
     return {
       statusCode: 401,
       body: JSON.stringify({
-        message: 'Invalid token',
+        message: "Invalid token",
       }),
     };
   }
 
   const { tenantId, refreshToken } = JSON.parse(event.body);
+  console.log(tenantId);
 
   try {
-    await cognito.createGroup({
-      GroupName: sub,
-      UserPoolId: userPoolId,
-      Description: `Group for user ${sub}`,
-    }).promise();
+    await cognito
+      .createGroup({
+        GroupName: sub,
+        UserPoolId: userPoolId,
+        Description: `Group for user ${sub}`,
+      })
+      .promise();
 
-    await cognito.adminAddUserToGroup({
-      GroupName: sub,
-      UserPoolId: userPoolId,
-      Username: sub,
-    }).promise();
+    await cognito
+      .adminAddUserToGroup({
+        GroupName: sub,
+        UserPoolId: userPoolId,
+        Username: sub,
+      })
+      .promise();
 
-    const authResponse = await cognito.initiateAuth({
-      AuthFlow: 'REFRESH_TOKEN_AUTH',
-      ClientId: clientId, 
-      AuthParameters: {
-        REFRESH_TOKEN: refreshToken,
-      },
-    }).promise();
-    console.log('Generated new tokens');
+    const authResponse = await cognito
+      .initiateAuth({
+        AuthFlow: "REFRESH_TOKEN_AUTH",
+        ClientId: clientId,
+        AuthParameters: {
+          REFRESH_TOKEN: refreshToken,
+        },
+      })
+      .promise();
+    console.log("Generated new tokens");
 
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'Group created, user added, attributes updated, and tokens refreshed',
+        message: "Group created, user added, attributes updated, and tokens refreshed",
         tokens: authResponse.AuthenticationResult,
       }),
     };
   } catch (error) {
     if (error instanceof Error) {
-      console.error('Error:', error.message);
+      console.error("Error:", error.message);
       return {
         statusCode: 500,
         body: JSON.stringify({
-          message: 'Error processing request',
+          message: "Error processing request",
           error: error.message,
         }),
       };
     } else {
       // Handle non-Error types of errors
-      console.error('Unknown error:', error);
+      console.error("Unknown error:", error);
       return {
         statusCode: 500,
         body: JSON.stringify({
-          message: 'An unknown error occurred',
-          error: 'Unknown error',
+          message: "An unknown error occurred",
+          error: "Unknown error",
         }),
       };
     }
-  }  
+  }
 };
