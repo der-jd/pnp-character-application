8e4697db8403deefaff40223ed52f121fdb6b058
der-jd <89513886+der-jd@users.noreply.github.com>
2024-12-22 14:58:07 +0100

    Use variable for AWS region and use full name for argument for better understanding

--- frontend/build_on_change.sh
+++ frontend/build_on_change.sh
@@ -2,48 +2,49 @@
 
 # TODO check if frontend is re-deployed when Cognito user pool has changed and therefore the env variables for the frontend have changed
 
+AWS_REGION="eu-central-1"
 CACHED_CHECKSUM="checksum.txt"
 
 # Check the frontend directory for changes and rebuild the frontend if changes are detected
-# Note: This excludes certain files of the frontend folder. 
+# Note: This excludes certain files of the frontend folder.
 # Note: If the s3 bucket is empty, an upload will take place even if the frontend did not change
 
 
 find . -type f \( \
     -path './src/*' \
-    -o -path './public/*' \
-    -o -path './lib/*' \
-    -o -path './components/*' \
-    -o -name 'src' \
-    -o -name 'public' \
-    -o -name 'lib' \
-    -o -name 'components' \) \
+    -or -path './public/*' \
+    -or -path './lib/*' \
+    -or -path './components/*' \
+    -or -name 'src' \
+    -or -name 'public' \
+    -or -name 'lib' \
+    -or -name 'components' \) \
     ! -name 'checksum.txt' \
     ! -name 'build_on_change.sh' \
     -exec md5sum {} \; > current_checksum.txt
-    
+
 if [ ! -f "$CACHED_CHECKSUM" ]; then
     echo "No Checksum file found, regenerating build!"
 else
     diff $CACHED_CHECKSUM current_checksum.txt > /dev/null
     if [ $? -eq 0 ]; then
         echo "No changes detected in frontend folder, checking bucket content!"
-        
+
         # The bucket should be at least accessible
-        if ! aws s3 ls s3://pnp-character-application-frontend --region eu-central-1 > /dev/null 2>&1; then
+        if ! aws s3 ls s3://pnp-character-application-frontend --region $AWS_REGION > /dev/null 2>&1; then
             echo "Bucket does not exist or access is restricted, please check terraform setup!"
             echo "Skipping build since the upload destination is inaccessible"
             exit 2
         fi
-        
-        BUCKET_EMPTY=$(aws s3 ls s3://pnp-character-application-frontend --region eu-central-1 --recursive)
+
+        BUCKET_EMPTY=$(aws s3 ls s3://pnp-character-application-frontend --region $AWS_REGION --recursive)
 
         # Bucket should be empty if recreated, in this case we want to build and deploy
         if [ -n "$BUCKET_EMPTY" ]; then
             echo "Bucket populated, skipping build!"
             exit 0
         fi
-        
+
         echo "Bucket is empty! Regenerating build files"
 
     else
@@ -55,7 +56,7 @@ rm -rf build
 npm run build
 mv out build
 
-aws s3 sync build s3://pnp-character-application-frontend --region eu-central-1
+aws s3 sync build s3://pnp-character-application-frontend --region $AWS_REGION
 
 rm -f checksum.txt
 mv current_checksum.txt checksum.txt
