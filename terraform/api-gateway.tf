9bb4d4d6b41045d4f959a6e7631595a3bfdb22e0
der-jd <89513886+der-jd@users.noreply.github.com>
2024-12-22 22:33:31 +0100

    Update Lambda name

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -26,27 +26,27 @@ resource "aws_api_gateway_integration" "increase_skill_integration" {
   uri                     = aws_lambda_function.increase_skill_lambda.invoke_arn
 }
 
-resource "aws_api_gateway_resource" "get_skill_cost_resource" {
+resource "aws_api_gateway_resource" "get_skill_increase_cost_resource" {
   rest_api_id = aws_api_gateway_rest_api.pnp_rest_api.id
   parent_id   = aws_api_gateway_rest_api.pnp_rest_api.root_resource_id
-  path_part   = "get-skill-cost"
+  path_part   = "get-skill-increase-cost"
 }
 
-resource "aws_api_gateway_method" "get_skill_cost_method" {
+resource "aws_api_gateway_method" "get_skill_increase_cost_method" {
   rest_api_id   = aws_api_gateway_rest_api.pnp_rest_api.id
-  resource_id   = aws_api_gateway_resource.get_skill_cost_resource.id
+  resource_id   = aws_api_gateway_resource.get_skill_increase_cost_resource.id
   http_method   = "GET"
   authorization = "COGNITO_USER_POOLS"
   authorizer_id = aws_api_gateway_authorizer.cognito_authorizer.id
 }
 
-resource "aws_api_gateway_integration" "get_skill_cost_integration" {
+resource "aws_api_gateway_integration" "get_skill_increase_cost_integration" {
   rest_api_id             = aws_api_gateway_rest_api.pnp_rest_api.id
-  resource_id             = aws_api_gateway_resource.get_skill_cost_resource.id
-  http_method             = aws_api_gateway_method.get_skill_cost_method.http_method
+  resource_id             = aws_api_gateway_resource.get_skill_increase_cost_resource.id
+  http_method             = aws_api_gateway_method.get_skill_increase_cost_method.http_method
   integration_http_method = "GET"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.get_skill_cost_lambda.invoke_arn
+  uri                     = aws_lambda_function.get_skill_increase_cost_lambda.invoke_arn
 }
 
 resource "aws_api_gateway_resource" "create_tenant_id_resource" {
@@ -76,7 +76,7 @@ resource "aws_api_gateway_deployment" "api_deployment" {
   depends_on = [
     aws_api_gateway_integration.increase_skill_integration,
     aws_api_gateway_integration.create_tenant_id_integration,
-    aws_api_gateway_integration.get_skill_cost_integration
+    aws_api_gateway_integration.get_skill_increase_cost_integration
   ]
   rest_api_id = aws_api_gateway_rest_api.pnp_rest_api.id
 }
