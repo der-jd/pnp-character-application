d685821b90643f55babab0d3a513f51748eed23e
Philipp Eich <philipp.eich1992@gmail.com>
2024-12-22 14:13:51 +0100

    skeleton for get skill cost lambda

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -26,6 +26,29 @@ resource "aws_api_gateway_integration" "increase_skill_integration" {
   uri                     = aws_lambda_function.increase_skill_lambda.invoke_arn
 }
 
+resource "aws_api_gateway_resource" "get_skill_cost_resource" {
+  rest_api_id = aws_api_gateway_rest_api.pnp_rest_api.id
+  parent_id   = aws_api_gateway_rest_api.pnp_rest_api.root_resource_id
+  path_part   = "get-skill-cost"
+}
+
+resource "aws_api_gateway_method" "get_skill_cost_method" {
+  rest_api_id   = aws_api_gateway_rest_api.pnp_rest_api.id
+  resource_id   = aws_api_gateway_resource.get_skill_cost_resource.id
+  http_method   = "GET"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.cognito_authorizer.id
+}
+
+resource "aws_api_gateway_integration" "get_skill_cost_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.pnp_rest_api.id
+  resource_id             = aws_api_gateway_resource.get_skill_cost_resource.id
+  http_method             = aws_api_gateway_method.get_skill_cost_method.http_method
+  integration_http_method = "GET"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.get_skill_cost_lambda.invoke_arn
+}
+
 resource "aws_api_gateway_resource" "create_tenant_id_resource" {
   rest_api_id = aws_api_gateway_rest_api.pnp_rest_api.id
   parent_id   = aws_api_gateway_rest_api.pnp_rest_api.root_resource_id
@@ -52,7 +75,9 @@ resource "aws_api_gateway_integration" "create_tenant_id_integration" {
 resource "aws_api_gateway_deployment" "api_deployment" {
   depends_on = [
     aws_api_gateway_integration.increase_skill_integration,
-    aws_api_gateway_integration.create_tenant_id_integration
+    aws_api_gateway_integration.get_character_integration,
+    aws_api_gateway_integration.create_tenant_id_integration,
+    aws_api_gateway_integration.get_skill_cost_integration
   ]
   rest_api_id = aws_api_gateway_rest_api.pnp_rest_api.id
 }
