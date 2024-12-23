d20a9b8cc598fd5fe2693b2dd6a1fff3ee38357a
Julian <89513886+der-jd@users.noreply.github.com>
2024-12-23 23:34:00 +0100

    Calculate cost category in backend (#26)
    Use learning method as input from frontend and calculate the adjusted
    cost category in the backend

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,2 +1,2 @@
-export { CostCategory, getSkillIncreaseCost } from "./cost.js";
+export { LearningMethod, CostCategory, getSkillIncreaseCost } from "./cost.js";
 export { Attribute, BaseValue, Skill, CombatSkill, Character, CharacterSheet, getSkill } from "./character.js";
