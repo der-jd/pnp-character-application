794d50c5018e014f312df840a38785c2adb86607
Philipp Eich <philipp.eich1992@gmail.com>
2024-12-23 17:32:08 +0100

    added error in case of no token

--- backend/src/lambdas/create-tenant-id/index.ts
+++ backend/src/lambdas/create-tenant-id/index.ts
@@ -2,76 +2,78 @@ import { CognitoIdentityProvider } from "@aws-sdk/client-cognito-identity-provid
 import * as jwt from "jsonwebtoken";
 const cognito = new CognitoIdentityProvider();
 export const handler = async (event) => {
-    const userPoolId = process.env.USER_POOL_ID;
-    const clientId = process.env.CLIENT_ID;
-    if (!userPoolId || !clientId) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: "User pool ID or client ID is not set in environment variables.",
-            }),
-        };
+  const userPoolId = process.env.USER_POOL_ID;
+  const clientId = process.env.CLIENT_ID;
+  if (!userPoolId || !clientId) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "User pool ID or client ID is not set in environment variables.",
+      }),
+    };
+  }
+
+  console.log("TESTTTTTTTTTTTTTTTTTTTTTTTT");
+  console.log(event);
+  console.log("TESTTTTTTTTTTTTTTTTTTTTTTTT");
+
+  const token = event.headers.Authorization.split(" ")[1];
+
+  console.log(token);
+
+  let sub;
+  try {
+    const decoded = jwt.decode(token);
+
+    if (!decoded) {
+      throw new Error("No token received!");
     }
 
-    console.log("TESTTTTTTTTTTTTTTTTTTTTTTTT")
-    console.log(event)
-    console.log("TESTTTTTTTTTTTTTTTTTTTTTTTT")
+    if (!decoded.sub) {
+      throw new Error("Token does not contain sub");
+    }
+    sub = decoded.sub;
+  } catch (error) {
+    console.error("Error decoding token:", error);
+    return {
+      statusCode: 401,
+      body: JSON.stringify({
+        message: "Invalid token",
+      }),
+    };
+  }
 
-    const token = event.headers.Authorization.split(" ")[1];
-
-    console.log(token);
-    
-    let sub;
-    try {
-        const decoded = jwt.decode(token);
-        if (!decoded.sub) {
-            throw new Error("Token does not contain sub");
-        }
-        sub = decoded.sub;
+  try {
+    await cognito.createGroup({
+      GroupName: sub,
+      UserPoolId: userPoolId,
+      Description: `Group for user ${sub}`,
+    });
+    await cognito.adminAddUserToGroup({
+      GroupName: sub,
+      UserPoolId: userPoolId,
+      Username: sub,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Error:", error.message);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: "Error processing request",
+          error: error.message,
+        }),
+      };
+    } else {
+      // Handle non-Error types of errors
+      console.error("Unknown error:", error);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: "An unknown error occurred",
+          error: "Unknown error",
+        }),
+      };
     }
-    catch (error) {
-        console.error("Error decoding token:", error);
-        return {
-            statusCode: 401,
-            body: JSON.stringify({
-                message: "Invalid token",
-            }),
-        };
-    }
-
-    try {
-        await cognito.createGroup({
-            GroupName: sub,
-            UserPoolId: userPoolId,
-            Description: `Group for user ${sub}`,
-        });
-        await cognito.adminAddUserToGroup({
-            GroupName: sub,
-            UserPoolId: userPoolId,
-            Username: sub,
-        });
-    }
-    catch (error) {
-        if (error instanceof Error) {
-            console.error("Error:", error.message);
-            return {
-                statusCode: 500,
-                body: JSON.stringify({
-                    message: "Error processing request",
-                    error: error.message,
-                }),
-            };
-        }
-        else {
-            // Handle non-Error types of errors
-            console.error("Unknown error:", error);
-            return {
-                statusCode: 500,
-                body: JSON.stringify({
-                    message: "An unknown error occurred",
-                    error: "Unknown error",
-                }),
-            };
-        }
-    }
+  }
 };
