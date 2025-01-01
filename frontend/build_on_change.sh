f5177ec4dee862e68c8ffb68937cbb0b2a8aa8bc
Philipp Eich <philipp.eich1992@gmail.com>
2025-01-01 10:45:14 +0100

    changed pipeline, more debug output

--- frontend/build_on_change.sh
+++ frontend/build_on_change.sh
@@ -40,7 +40,10 @@ fi
 if [ ! -f "$CACHED_CHECKSUM" ]; then
     echo "No Checksum file found, regenerating build!"
 else
-    echo "Current checksum: $(cat current_checksum.txt) old checksum: $CACHED_CHECKSUM"
+
+    echo "Current checksum: $(md5sum current_checksum.txt)"
+    echo "Old checksum: $(md5sum $CACHED_CHECKSUM)"
+
     diff $CACHED_CHECKSUM current_checksum.txt > /dev/null
     if [ $? -eq 0 ]; then
         echo "No changes detected in frontend folder, checking bucket content!"
