f2eaee99a92259a65914e233dd88bd19ca11845e
der-jd <89513886+der-jd@users.noreply.github.com>
2025-03-02 22:30:18 +0100

    Update character values
    Consolidate skills, etc. Add missing ones.

--- backend/src/config/character.ts
+++ backend/src/config/character.ts
@@ -9,12 +9,15 @@ export interface Attribute {
   start: number;
   current: number;
   mod: number;
+  totalCost: number;
 }
 
 export interface BaseValue {
   start: number;
   current: number;
+  byLvlUp: number;
   mod: number;
+  totalCost: number;
 }
 
 export interface Skill {
@@ -39,11 +42,11 @@ export interface CharacterSheet {
     sex: string;
     profession: {
       name: string;
-      Skill: string;
+      skill: string;
     };
     hobby: {
       name: string;
-      Skill: string;
+      skill: string;
     };
     birthday: string;
     birthplace: string;
@@ -57,6 +60,7 @@ export interface CharacterSheet {
   };
   calculationPoints: {
     adventurePoints: {
+      start: number;
       available: number;
       total: number;
     };
@@ -68,6 +72,20 @@ export interface CharacterSheet {
   };
   advantages: string[];
   disadvantages: string[];
+  specialAbilities: string[];
+  baseValues: {
+    healthPoints: BaseValue;
+    mentalHealth: BaseValue;
+    armorLevel: BaseValue;
+    naturalArmor: BaseValue;
+    initiativeBaseValue: BaseValue;
+    attackBaseValue: BaseValue;
+    paradeBaseValue: BaseValue;
+    rangedAttackBaseValue: BaseValue;
+    luckPoints: BaseValue;
+    bonusActionsPerCombatRound: BaseValue;
+    legendaryActions: BaseValue;
+  };
   attributes: {
     courage: Attribute;
     intelligence: Attribute;
@@ -78,54 +96,117 @@ export interface CharacterSheet {
     endurance: Attribute;
     strength: Attribute;
   };
-  baseValues: {
-    healthPoints: BaseValue;
-    mentalHealth: BaseValue;
-    armorLevel: BaseValue;
-    initiativeBaseValue: BaseValue;
-    attackBaseValue: BaseValue;
-    paradeBaseValue: BaseValue;
-    rangedAttackBaseValue: BaseValue;
-    luckPoints: BaseValue;
-    bonusActionsPerCombatRound: BaseValue;
-    legendaryActions: BaseValue;
-  };
+  // TODO consolidate skills. Some of them are too special and probably never used
   skills: {
     combat: {
+      martialArts: Skill;
+      barehanded: Skill;
+      chainWeapons: Skill;
+      daggers: Skill;
+      slashingWeapons1h: Skill;
+      thrustingWeapons1h: Skill;
+      slashingWeapons2h: Skill;
+      thrustingWeapons2h: Skill;
+      polearms: Skill;
       greatsword: Skill;
-      martialArt: Skill;
-      firearmSimple: Skill;
       missile: Skill;
+      firearmSimple: Skill;
+      firearmMedium: Skill;
+      firearmComplex: Skill;
     };
     body: {
       athletics: Skill;
+      juggleries: Skill;
+      climbing: Skill;
+      bodyControl: Skill;
+      riding: Skill;
+      sneaking: Skill;
       swimming: Skill;
+      selfControl: Skill;
+      hiding: Skill;
+      singing: Skill;
+      sharpnessOfSenses: Skill;
+      imitatingVoices: Skill;
+      dancing: Skill;
+      quaffing: Skill;
+      pickpocketing: Skill;
     };
     social: {
+      seduction: Skill;
+      etiquette: Skill;
+      teaching: Skill;
       acting: Skill;
+      writtenExpression: Skill;
+      disguising: Skill;
+      streetKnowledge: Skill;
+      knowledgeOfHumanNature: Skill;
+      persuading: Skill;
       convincing: Skill;
     };
     nature: {
-      fishing: Skill;
+      tracking: Skill;
+      knottingSkills: Skill;
+      trapping: Skill;
+      fishingAnd: Skill;
       orientation: Skill;
+      wildernessLife: Skill;
     };
     knowledge: {
+      anatomy: Skill;
+      architecture: Skill;
       geography: Skill;
       history: Skill;
+      petrology: Skill;
+      botany: Skill;
+      philosophy: Skill;
+      astronomy: Skill;
+      mathematics: Skill;
+      knowledgeOfTheLaw: Skill;
+      estimating: Skill;
+      zoology: Skill;
+      technology: Skill;
+      chemistry: Skill;
+      warfare: Skill;
+      itSkills: Skill;
+      mechanics: Skill;
     };
     handcraft: {
-      butcher: Skill;
+      training: Skill;
+      woodwork: Skill;
+      foodProcessing: Skill;
+      leatherProcessing: Skill;
+      metalwork: Skill;
+      stoneWork: Skill;
+      fabricProcessing: Skill;
+      alcoholProduction: Skill;
+      steeringVehicles: Skill;
+      cheating: Skill;
+      bargaining: Skill;
+      firstAid: Skill;
+      calmingSbDown: Skill;
+      drawingAndPainting: Skill;
+      makingMusic: Skill;
       lockpicking: Skill;
     };
   };
   combatSkills: {
     melee: {
+      martialArts: CombatSkill;
+      barehanded: CombatSkill;
+      chainWeapons: CombatSkill;
+      daggers: CombatSkill;
+      slashingWeapons1h: CombatSkill;
+      thrustingWeapons1h: CombatSkill;
+      slashingWeapons2h: CombatSkill;
+      thrustingWeapons2h: CombatSkill;
+      polearms: CombatSkill;
       greatsword: CombatSkill;
-      martialArt: CombatSkill;
     };
     ranged: {
-      firearmSimple: CombatSkill;
       missile: CombatSkill;
+      firearmSimple: CombatSkill;
+      firearmMedium: CombatSkill;
+      firearmComplex: CombatSkill;
     };
   };
 }
