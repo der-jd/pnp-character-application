b9f75909eb450df142e93697baa3b654b80d1246
der-jd <89513886+der-jd@users.noreply.github.com>
2025-03-07 00:02:31 +0100

    Add mapping for char id, general information, attributes, base values, calc points. ad- and disadvantes

--- scripts/generate_json_schema.ts
+++ scripts/generate_json_schema.ts
@@ -5,7 +5,7 @@ import { parseString, parseStringPromise } from "xml2js"; // xml2js for parsing 
 import { Character } from "../backend/src/config/character.js";
 import { Attribute } from "../backend/src/config/character.js";
 import { sample_char } from "./sample_char.js";
-import yargs from "yargs";
+import yargs, { parsed } from "yargs";
 import { hideBin } from "yargs/helpers";
 import { v4 as uuidv4 } from "uuid";
 import unidecode from "unidecode"; // unidecode for converting Unicode to ASCII
@@ -61,7 +61,7 @@ interface MyData {
 }
 
 // Function to read XML file, map it to the interface, and handle Unicode
-async function readAndMapXmlToInterface(filePath: string): Promise<Attribute> {
+async function readAndMapXmlToInterface(filePath: string): Promise<Character> {
   console.log("Current working directory:", process.cwd());
   console.log("Reading file:", filePath);
   if (!fs.existsSync(filePath)) {
@@ -98,10 +98,196 @@ async function readAndMapXmlToInterface(filePath: string): Promise<Attribute> {
   console.log(parsedData.attributes[0]);
   console.log("====");
   return {
-    start: parsedData.attributes[0].Mutu0020u0028MUu0029.start?.[0] || 0,
-    current: parsedData.attributes[0].Mutu0020u0028MUu0029.current?.[0] || 0,
-    mod: parsedData.attributes[0].Mutu0020u0028MUu0029.mod?.[0] || 0,
-    totalCost: parsedData.attributes[0].Mutu0020u0028MUu0029.totalCost?.[0] || 0,
+    characterId: uuidv4(),
+    characterSheet: {
+      generalInformation: {
+        name: convertToAscii(parsedData.general_information[0].name?.[0] || ""),
+        level: parsedData.level?.[0] || 0,
+        sex: convertToAscii(parsedData.general_information[0].sex?.[0] || ""),
+        profession: {
+          name: convertToAscii(parsedData.general_information[0].profession[0].name?.[0] || ""),
+          skill: convertToAscii(parsedData.general_information[0].profession[0].skill?.[0] || ""),
+        },
+        hobby: {
+          name: convertToAscii(parsedData.general_information[0].hobby[0].name?.[0] || ""),
+          skill: convertToAscii(parsedData.general_information[0].hobby[0].skill?.[0] || ""),
+        },
+        birthday: convertToAscii(parsedData.general_information[0].birthday?.[0] || ""),
+        birthplace: convertToAscii(parsedData.general_information[0].birthplace?.[0] || ""),
+        size: convertToAscii(parsedData.general_information[0].size?.[0] || ""),
+        weight: convertToAscii(parsedData.general_information[0].weight?.[0] || ""),
+        hairColor: convertToAscii(parsedData.general_information[0].hair_color?.[0] || ""),
+        eyeColor: convertToAscii(parsedData.general_information[0].eye_color?.[0] || ""),
+        residence: convertToAscii(parsedData.general_information[0].residence?.[0] || ""),
+        specialCharacteristics: convertToAscii(parsedData.general_information[0].special_characteristics?.[0] || ""),
+        appearance: convertToAscii(parsedData.general_information[0].appearance?.[0] || ""),
+      },
+      calculationPoints: {
+        adventurePoints: {
+          start: 1000,
+          available: parsedData.calculation_points[0].adventure_points[0].available?.[0] || 0,
+          total: parsedData.calculation_points[0].adventure_points[0].total?.[0] || 0,
+        },
+        attributePoints: {
+          start: 40,
+          available: (40 + parsedData.calculation_points[0].attribute_points[0].additional?.[0] || 0) - parsedData.calculation_points[0].attribute_points[0].spent?.[0] || 0,
+          total: 40 + parsedData.calculation_points[0].attribute_points[0].additional?.[0] || 0,
+        },
+      },
+      advantages: parsedData.advantages[0].advantage?.map((adv: any) => convertToAscii(adv)) || [],
+      disadvantages: parsedData.disadvantages[0].disadvantage?.map((dis: any) => convertToAscii(dis)) || [],
+      specialAbilities: [],
+      baseValues: {
+        healthPoints: {
+          start: parsedData.base_points[0].Lebenspunkteu0020u0028LePu0029[0].start?.[0] || 0,
+          current: parsedData.base_points[0].Lebenspunkteu0020u0028LePu0029[0].current?.[0] || 0,
+          byLvlUp: parsedData.base_points[0].Lebenspunkteu0020u0028LePu0029[0].bought?.[0] || 0,
+          mod: parsedData.base_points[0].Lebenspunkteu0020u0028LePu0029[0].mod?.[0] || 0,
+          totalCost: parsedData.base_points[0].Lebenspunkteu0020u0028LePu0029[0].total_cost?.[0] || 0,
+        },
+        mentalHealth: {
+          start: parsedData.base_points[0].Geistigeu0020Gesundheitu0020u0028GGu0029[0].start?.[0] || 0,
+          current: parsedData.base_points[0].Geistigeu0020Gesundheitu0020u0028GGu0029[0].current?.[0] || 0,
+          byLvlUp: parsedData.base_points[0].Geistigeu0020Gesundheitu0020u0028GGu0029[0].bought?.[0] || 0,
+          mod: parsedData.base_points[0].Geistigeu0020Gesundheitu0020u0028GGu0029[0].mod?.[0] || 0,
+          totalCost: parsedData.base_points[0].Geistigeu0020Gesundheitu0020u0028GGu0029[0].total_cost?.[0] || 0,
+        },
+        armorLevel: {
+          start: parsedData.base_points[0].Rüstungslevel[0].start?.[0] || 0,
+          current: parsedData.base_points[0].Rüstungslevel[0].current?.[0] || 0,
+          byLvlUp: parsedData.base_points[0].Rüstungslevel[0].bought?.[0] || 0,
+          mod: parsedData.base_points[0].Rüstungslevel[0].mod?.[0] || 0,
+          totalCost: parsedData.base_points[0].Rüstungslevel[0].total_cost?.[0] || 0,
+        },
+        naturalArmor: {
+          start: 0,
+          current: 0,
+          byLvlUp: 0,
+          mod: 0,
+          totalCost: 0,
+        },
+        initiativeBaseValue: {
+          start: parsedData.base_points[0]["INI-Basiswert"][0].start?.[0] || 0,
+          current: parsedData.base_points[0]["INI-Basiswert"][0].current?.[0] || 0,
+          byLvlUp: parsedData.base_points[0]["INI-Basiswert"][0].bought?.[0] || 0,
+          mod: parsedData.base_points[0]["INI-Basiswert"][0].mod?.[0] || 0,
+          totalCost: parsedData.base_points[0]["INI-Basiswert"][0].total_cost?.[0] || 0,
+        },
+        attackBaseValue: {
+          start: parsedData.base_points[0]["AT-Basiswert"][0].start?.[0] || 0,
+          current: parsedData.base_points[0]["AT-Basiswert"][0].current?.[0] || 0,
+          byLvlUp: parsedData.base_points[0]["AT-Basiswert"][0].bought?.[0] || 0,
+          mod: parsedData.base_points[0]["AT-Basiswert"][0].mod?.[0] || 0,
+          totalCost: parsedData.base_points[0]["AT-Basiswert"][0].total_cost?.[0] || 0,
+        },
+        paradeBaseValue: {
+          start: parsedData.base_points[0]["PA-Basiswert"][0].start?.[0] || 0,
+          current: parsedData.base_points[0]["PA-Basiswert"][0].current?.[0] || 0,
+          byLvlUp: parsedData.base_points[0]["PA-Basiswert"][0].bought?.[0] || 0,
+          mod: parsedData.base_points[0]["PA-Basiswert"][0].mod?.[0] || 0,
+          totalCost: parsedData.base_points[0]["PA-Basiswert"][0].total_cost?.[0] || 0,
+        },
+        rangedAttackBaseValue: {
+          start: parsedData.base_points[0]["FK-Basiswert"][0].start?.[0] || 0,
+          current: parsedData.base_points[0]["FK-Basiswert"][0].current?.[0] || 0,
+          byLvlUp: parsedData.base_points[0]["FK-Basiswert"][0].bought?.[0] || 0,
+          mod: parsedData.base_points[0]["FK-Basiswert"][0].mod?.[0] || 0,
+          totalCost: parsedData.base_points[0]["FK-Basiswert"][0].total_cost?.[0] || 0,
+        },
+        luckPoints: {
+          start: parsedData.base_points[0].Glückspunkte[0].start?.[0] || 0,
+          current: parsedData.base_points[0].Glückspunkte[0].current?.[0] || 0,
+          byLvlUp: parsedData.base_points[0].Glückspunkte[0].bought?.[0] || 0,
+          mod: parsedData.base_points[0].Glückspunkte[0].mod?.[0] || 0,
+          totalCost: parsedData.base_points[0].Glückspunkte[0].total_cost?.[0] || 0,
+        },
+        bonusActionsPerCombatRound: {
+          start: parsedData.base_points[0].Bonusaktionenu0020prou0020KR[0].start?.[0] || 0,
+          current: parsedData.base_points[0].Bonusaktionenu0020prou0020KR[0].current?.[0] || 0,
+          byLvlUp: parsedData.base_points[0].Bonusaktionenu0020prou0020KR[0].bought?.[0] || 0,
+          mod: parsedData.base_points[0].Bonusaktionenu0020prou0020KR[0].mod?.[0] || 0,
+          totalCost: parsedData.base_points[0].Bonusaktionenu0020prou0020KR[0].total_cost?.[0] || 0,
+        },
+        legendaryActions: {
+          start: parsedData.base_points[0].Legendäreu0020Aktionen[0].start?.[0] || 0,
+          current: parsedData.base_points[0].Legendäreu0020Aktionen[0].current?.[0] || 0,
+          byLvlUp: parsedData.base_points[0].Legendäreu0020Aktionen[0].bought?.[0] || 0,
+          mod: parsedData.base_points[0].Legendäreu0020Aktionen[0].mod?.[0] || 0,
+          totalCost: parsedData.base_points[0].Legendäreu0020Aktionen[0].total_cost?.[0] || 0,
+        },
+      },
+      attributes: {
+        courage: {
+          start: parsedData.attributes[0].Mutu0020u0028MUu0029.start?.[0] || 0,
+          current: parsedData.attributes[0].Mutu0020u0028MUu0029.current?.[0] || 0,
+          mod: parsedData.attributes[0].Mutu0020u0028MUu0029.mod?.[0] || 0,
+          totalCost: parsedData.attributes[0].Mutu0020u0028MUu0029.totalCost?.[0] || 0,
+        },
+        intelligence: {
+          start: parsedData.attributes[0].Klugheitu0020u0028KLu0029.start?.[0] || 0,
+          current: parsedData.attributes[0].Klugheitu0020u0028KLu0029.current?.[0] || 0,
+          mod: parsedData.attributes[0].Klugheitu0020u0028KLu0029.mod?.[0] || 0,
+          totalCost: parsedData.attributes[0].Klugheitu0020u0028KLu0029.totalCost?.[0] || 0,
+        },
+        concentration: {
+          start: parsedData.attributes[0].Konzentrationu0020u0028KOu0029.start?.[0] || 0,
+          current: parsedData.attributes[0].Konzentrationu0020u0028KOu0029.current?.[0] || 0,
+          mod: parsedData.attributes[0].Konzentrationu0020u0028KOu0029.mod?.[0] || 0,
+          totalCost: parsedData.attributes[0].Konzentrationu0020u0028KOu0029.totalCost?.[0] || 0,
+        },
+        charisma: {
+          start: parsedData.attributes[0].Charismau0020u0028CHu0029.start?.[0] || 0,
+          current: parsedData.attributes[0].Charismau0020u0028CHu0029.current?.[0] || 0,
+          mod: parsedData.attributes[0].Charismau0020u0028CHu0029.mod?.[0] || 0,
+          totalCost: parsedData.attributes[0].Charismau0020u0028CHu0029.totalCost?.[0] || 0,
+        },
+        mentalResilience: {
+          start: parsedData.attributes[0].Mentaleu0020Belastbarkeitu0020u0028MBu0029.start?.[0] || 0,
+          current: parsedData.attributes[0].Mentaleu0020Belastbarkeitu0020u0028MBu0029.current?.[0] || 0,
+          mod: parsedData.attributes[0].Mentaleu0020Belastbarkeitu0020u0028MBu0029.mod?.[0] || 0,
+          totalCost: parsedData.attributes[0].Mentaleu0020Belastbarkeitu0020u0028MBu0029.totalCost?.[0] || 0,
+        },
+        dexterity: {
+          start: parsedData.attributes[0].Geschicklichkeitu0020u0028GEu0029.start?.[0] || 0,
+          current: parsedData.attributes[0].Geschicklichkeitu0020u0028GEu0029.current?.[0] || 0,
+          mod: parsedData.attributes[0].Geschicklichkeitu0020u0028GEu0029.mod?.[0] || 0,
+          totalCost: parsedData.attributes[0].Geschicklichkeitu0020u0028GEu0029.totalCost?.[0] || 0,
+        },
+        endurance: {
+          start: parsedData.attributes[0].Ausdaueru0020u0028AUu0029.start?.[0] || 0,
+          current: parsedData.attributes[0].Ausdaueru0020u0028AUu0029.current?.[0] || 0,
+          mod: parsedData.attributes[0].Ausdaueru0020u0028AUu0029.mod?.[0] || 0,
+          totalCost: parsedData.attributes[0].Ausdaueru0020u0028AUu0029.totalCost?.[0] || 0,
+        },
+        strength: {
+          start: parsedData.attributes[0].Kraftu0020u0028KRu0029.start?.[0] || 0,
+          current: parsedData.attributes[0].Kraftu0020u0028KRu0029.current?.[0] || 0,
+          mod: parsedData.attributes[0].Kraftu0020u0028KRu0029.mod?.[0] || 0,
+          totalCost: parsedData.attributes[0].Kraftu0020u0028KRu0029.totalCost?.[0] || 0,
+        },
+      },
+      skills: { // TODO
+        combat: {
+        },
+        body: {
+        },
+        social: {
+        },
+        nature: {
+        },
+        knowledge: {
+        },
+        handcraft: {
+        },
+      },
+      combatSkills: { // TODO
+        melee: {
+
+        },
+        ranged: {
+        },
+      }
+    },
   };
 }
 
@@ -111,7 +297,7 @@ const argv = yargs(hideBin(process.argv))
     alias: "i",
     type: "string",
     demandOption: true,
-    describe: "Path to input XML file",
+    describe: "Path to input XML file (v6.1)",
   })
   .option("output", {
     alias: "o",
@@ -125,7 +311,7 @@ const argv = yargs(hideBin(process.argv))
 // Main function to execute the logic
 (async () => {
   try {
-    const character: Attribute = await readAndMapXmlToInterface(argv.input); // Await the result
+    const character: Character = await readAndMapXmlToInterface(argv.input);
 
     // Convert the character data to a formatted JSON string
     let jsonString = JSON.stringify(character, null, 2);
@@ -139,6 +325,15 @@ const argv = yargs(hideBin(process.argv))
     const jsonString2 = JSON.stringify(marshalled, null, 2);
     fs.writeFileSync(`${argv.output}_dynamodb.json`, jsonString2);
     console.log("DynamoDB JSON saved to:", argv.output + "_dynamodb.json");
+
+    console.warn("Warning: Check the generated JSON file for correctness!");
+
+    console.warn("Warning: The following fields need to be filled out manually:");
+    console.warn("- characterSheet.calculationPoints.adventurePoints.available (number)");
+    console.warn("- characterSheet.specialAbilities (string[])");
+    console.warn("- characterSheet.attributes.XXX.totalCost (number)");
+    console.warn("- characterSheet.baseValues.naturalArmor (number)");
+    console.warn("- characterSheet.baseValues.XXX.current (number)");
   } catch (error) {
     console.error("Error:", error);
   }
