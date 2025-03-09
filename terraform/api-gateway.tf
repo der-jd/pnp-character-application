596fc20f8a81407313c053dac05475524de57a60
der-jd <89513886+der-jd@users.noreply.github.com>
2025-03-09 21:26:18 +0100

    fix: Use POST for Lambda integration; add CORS

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -33,9 +33,64 @@ resource "aws_api_gateway_integration" "characters_get_integration" {
   rest_api_id             = aws_api_gateway_rest_api.pnp_rest_api.id
   resource_id             = aws_api_gateway_resource.characters.id
   http_method             = aws_api_gateway_method.characters_get.http_method
-  integration_http_method = "GET"
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.get_characters_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_method" "characters_options" {
+  rest_api_id = aws_api_gateway_rest_api.pnp_rest_api.id
+  resource_id = aws_api_gateway_resource.characters.id
+  http_method = "OPTIONS"
+  // Authorization needs to be NONE for the preflight request to work which is sent automatically by the browser without any authorization header.
+  authorization = "NONE"
+  request_parameters = {
+    "method.request.header.Origin" : false
+  }
+}
+
+resource "aws_api_gateway_integration" "characters_options_integration" {
+  rest_api_id = aws_api_gateway_rest_api.pnp_rest_api.id
+  resource_id = aws_api_gateway_resource.characters.id
+  http_method = aws_api_gateway_method.characters_options.http_method
+  type        = "MOCK"
+  // see https://docs.aws.amazon.com/apigateway/latest/developerguide/how-to-mock-integration.html#how-to-mock-integration-request-examples
+  // For a method with the mock integration to return a 200 response, configure the
+  // integration request body mapping template to return the following:
+  request_templates = {
+    "application/json" = jsonencode(
+      {
+        statusCode = 200
+      }
+    )
+  }
+}
+
+resource "aws_api_gateway_integration_response" "characters_options_integration_response" {
+  depends_on = [aws_api_gateway_integration.characters_options_integration]
+
+  rest_api_id = aws_api_gateway_rest_api.pnp_rest_api.id
+  resource_id = aws_api_gateway_resource.characters.id
+  http_method = aws_api_gateway_method.characters_options.http_method
+  status_code = 200
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'" // TODO delete after testing and comment in following line
+    //"method.response.header.Access-Control-Allow-Origin"  = "'https://${aws_cloudfront_distribution.frontend_distribution.domain_name}'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,GET'"
+  }
+}
+
+resource "aws_api_gateway_method_response" "characters_options_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.pnp_rest_api.id
+  resource_id = aws_api_gateway_resource.characters.id
+  http_method = aws_api_gateway_method.characters_options.http_method
+  status_code = 200
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "true"
+    "method.response.header.Access-Control-Allow-Methods" = "true"
+    "method.response.header.Access-Control-Allow-Origin"  = "true"
+  }
 }
 
 resource "aws_api_gateway_resource" "character_id" {
@@ -595,6 +650,7 @@ resource "aws_api_gateway_deployment" "api_deployment" {
   depends_on = [
     aws_api_gateway_method.character_id_get,
     aws_api_gateway_method_response.character_id_get_method_response,
+    aws_api_gateway_integration.characters_get_integration,
     aws_api_gateway_integration.character_id_get_integration,
     aws_api_gateway_integration_response.character_id_get_integration_response,
     aws_api_gateway_method.skill_name_get,
