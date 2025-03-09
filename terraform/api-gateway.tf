b87ddc5f818bce49c9ebce524af46f976531bc90
Philipp Eich <philipp.eich1992@gmail.com>
2025-03-09 15:08:29 +0100

    changed method to post

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -37,7 +37,7 @@ resource "aws_api_gateway_integration" "character_id_get_integration" {
   rest_api_id             = aws_api_gateway_rest_api.pnp_rest_api.id
   resource_id             = aws_api_gateway_resource.character_id.id
   http_method             = aws_api_gateway_method.character_id_get.http_method
-  integration_http_method = "GET"
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.get_character_lambda.invoke_arn
   request_parameters = {
