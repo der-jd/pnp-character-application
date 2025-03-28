e985db635d1fa5270e748be34b8aa3a838e0788b
der-jd <89513886+der-jd@users.noreply.github.com>
2025-03-28 11:07:08 +0100

    Validate input data

--- backend/src/lambdas/add-event-to-history/index.ts
+++ backend/src/lambdas/add-event-to-history/index.ts
@@ -1,10 +1,19 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
-import { Event } from "config/index.js";
+import { z } from "zod";
+import { RecordType } from "config/index.js";
+import {
+  attributeSchema,
+  baseValueSchema,
+  calculationPointsSchema,
+  combatSkillSchema,
+  professionHobbySchema,
+  skillSchema,
+} from "config/character_schemas.js";
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  return addEventToHistory(event);
+  return addRecordToHistory(event);
 };
 
 interface historyBlock {
@@ -12,22 +21,56 @@ interface historyBlock {
   blockId: string;
   blockNumber: number;
   previousBlockId: string;
-  changes: Change[];
+  nextBlockId: string;
+  changes: Record[];
 }
 
-interface Change {
+interface CalculationPointsChange {
+  adjustment: number;
+  old: number;
+  new: number;
+}
+
+const bodySchema = z.object({
+  type: z.string(),
+  name: z.string(),
+  data: z.object({
+    old: z.record(z.any()),
+    new: z.record(z.any()),
+  }),
+  learningMethod: z.string(),
+  calculationPointsChange: z.object({
+    adjustment: z.number(),
+    old: z.number(),
+    new: z.number(),
+  }),
+  comment: z.string(),
+});
+
+const levelSchema = z.object({
+  value: z.number(),
+});
+
+const stringSchema = z.object({
+  value: z.string(),
+});
+
+const booleanSchema = z.object({
+  value: z.boolean(),
+});
+
+// TODO create schema for record (without to be generated data) and use it for input body validation?!
+interface Record {
   id: string; // To be generated in this function
-  event: Event; // input param
+  type: RecordType; // input param
   name: string; // input param
   // input param
-  data: { [key: string]: any }; // TODO we should use a fixed schema here to validate the incoming data to this function?!
+  data: {
+    old: { [key: string]: any };
+    new: { [key: string]: any };
+  }; // TODO we should use a fixed schema here to validate the incoming data to this function?!
   learningMethod: string; // input param
-  calculationPoints: {
-    // input param
-    adjustment: number;
-    old: number;
-    new: number;
-  };
+  calculationPointsChange: CalculationPointsChange; // input param
   // to be generated in this function
   timestamp: string; // YYYY-MM-DDThh:mm:ssZ/±hh:mm, e.g. 2025-03-24T16:34:56Z (UTC) or 2025-03-24T16:34:56+02:00
   comment: string; // input param
@@ -38,16 +81,17 @@ interface Parameters {
   characterId: string;
 }
 
-async function addEventToHistory(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+async function addRecordToHistory(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
   try {
     // TODO only allow access to the character history if the user has access to the character
-    const params = verifyRequest(event);
+    const params = validateRequest(event);
 
     const h: historyBlock = {
       characterId: "123",
       blockId: "123",
       blockNumber: 1,
       previousBlockId: "123",
+      nextBlockId: "153",
       changes: [],
     };
     console.log(h);
@@ -104,17 +148,10 @@ async function addEventToHistory(event: APIGatewayProxyEvent): Promise<APIGatewa
   }
 }
 
-function verifyRequest(event: APIGatewayProxyEvent): Parameters {
-  console.log("Verify request");
+function validateRequest(event: APIGatewayProxyEvent): Parameters {
+  console.log("Validate request");
 
-  // The conditional parse is necessary for Lambda tests via the AWS console
-  const body = typeof event.body === "string" ? JSON.parse(event.body) : event.body;
-  if (
-    typeof event.pathParameters?.["character-id"] !== "string" ||
-    typeof body?.initialValue !== "number" ||
-    typeof body?.increasedPoints !== "number" ||
-    typeof body?.learningMethod !== "string"
-  ) {
+  if (typeof event.pathParameters?.["character-id"] !== "string") {
     console.error("Invalid input values!");
     throw {
       statusCode: 400,
@@ -124,8 +161,85 @@ function verifyRequest(event: APIGatewayProxyEvent): Parameters {
     };
   }
 
-  const characterId = event.pathParameters?.["character-id"];
+  try {
+    // The conditional parse is necessary for Lambda tests via the AWS console
+    const body = typeof event.body === "string" ? JSON.parse(event.body) : event.body;
+    bodySchema.parse(body);
 
+    switch (RecordType.parse(body.type)) {
+      case RecordType.EVENT_CALCULATION_POINTS:
+        calculationPointsSchema.parse(body.data.old);
+        calculationPointsSchema.parse(body.data.new);
+        break;
+      case RecordType.EVENT_LEVEL_UP:
+        levelSchema.parse(body.data.old);
+        levelSchema.parse(body.data.new);
+        break;
+      case RecordType.EVENT_BASE_VALUE:
+        baseValueSchema.parse(body.data.old);
+        baseValueSchema.parse(body.data.new);
+        break;
+      case RecordType.PROFESSION_CHANGED:
+        professionHobbySchema.parse(body.data.old);
+        professionHobbySchema.parse(body.data.new);
+        break;
+      case RecordType.HOBBY_CHANGED:
+        professionHobbySchema.parse(body.data.old);
+        professionHobbySchema.parse(body.data.new);
+        break;
+      case RecordType.ADVANTAGE_CHANGED:
+      case RecordType.DISADVANTAGE_CHANGED:
+      case RecordType.SPECIAL_ABILITY_CHANGED:
+        stringSchema.parse(body.data.old);
+        stringSchema.parse(body.data.new);
+        break;
+      case RecordType.ATTRIBUTE_RAISED:
+        attributeSchema.parse(body.data.old);
+        attributeSchema.parse(body.data.new);
+        break;
+      case RecordType.SKILL_ACTIVATED:
+        booleanSchema.parse(body.data.old);
+        booleanSchema.parse(body.data.new);
+        break;
+      case RecordType.SKILL_RAISED:
+        skillSchema.parse(body.data.old);
+        skillSchema.parse(body.data.new);
+        break;
+      case RecordType.ATTACK_PARADE_DISTRIBUTED:
+        combatSkillSchema.parse(body.data.old);
+        combatSkillSchema.parse(body.data.new);
+        break;
+      default:
+        console.error("Invalid history record type!");
+        throw {
+          statusCode: 400,
+          body: JSON.stringify({
+            message: "Invalid history record type!",
+          }),
+        };
+    }
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      console.error("Validation errors:", error.errors);
+      throw {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: "Invalid input values!",
+          errors: error.errors,
+        }),
+      };
+    } else {
+      console.error("Unknown validation error:", error);
+      throw {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: "Unknown validation error for input values!",
+        }),
+      };
+    }
+  }
+
+  const characterId = event.pathParameters?.["character-id"];
   const uuidRegex = new RegExp("^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$");
   if (!uuidRegex.test(characterId)) {
     console.error("Character id is not a valid UUID format!");
@@ -138,7 +252,7 @@ function verifyRequest(event: APIGatewayProxyEvent): Parameters {
   }
 
   return {
-    userId: userId,
+    userId: "userId",
     characterId: characterId,
   };
 }
