01022313842fd040a74c05a180c43adc5340a157
Philipp Eich <philipp.eich1992@gmail.com>
2025-03-29 15:46:25 +0100

    added provider to layout

--- frontend/src/app/protected/layout.tsx
+++ frontend/src/app/protected/layout.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import Header from "@/components/Header";
+import { ToastProvider } from "../global/Notifications/notificationContext";
 
 export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Header />
-      <main>{children}</main>
+      <ToastProvider>
+        <Header />
+        <main>{children}</main>
+      </ToastProvider>
     </>
   );
 }
