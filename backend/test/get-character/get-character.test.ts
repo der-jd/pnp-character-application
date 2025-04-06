c3a374ccf55aeed0a1b9c78dac83adaa29562c73
der-jd <89513886+der-jd@users.noreply.github.com>
2025-04-06 21:57:56 +0200

    Add tests for get-character

---
+++ backend/test/get-character/get-character.test.ts
@@ -0,0 +1,109 @@
+import { describe, expect, test } from "vitest";
+import { fakeHeaders, dummyHeaders, fakeUserId } from "../test-data/request.js";
+import { fakeDynamoDBCharacterResponse, mockDynamoDBGetResponse } from "../test-data/response.js";
+import { fakeCharacterId } from "../test-data/character.js";
+import { getCharacter } from "../../src/lambdas/get-character/index.js";
+
+describe("Invalid requests", () => {
+  const invalidTestCases = [
+    {
+      name: "Authorization header is malformed",
+      request: {
+        headers: {
+          authorization: "dummyValue",
+        },
+        pathParameters: {
+          "character-id": fakeCharacterId,
+        },
+        body: null,
+      },
+      expectedStatusCode: 401,
+    },
+    {
+      name: "Authorization token is invalid",
+      request: {
+        headers: {
+          authorization: "Bearer 1234567890",
+        },
+        pathParameters: {
+          "character-id": fakeCharacterId,
+        },
+        body: null,
+      },
+      expectedStatusCode: 401,
+    },
+    {
+      name: "Character id is not an UUID",
+      request: {
+        headers: fakeHeaders,
+        pathParameters: {
+          "character-id": "1234567890",
+        },
+        body: null,
+      },
+      expectedStatusCode: 400,
+    },
+    {
+      name: "No character found for a non existing character id",
+      request: {
+        headers: fakeHeaders,
+        pathParameters: {
+          "character-id": "26c5d41d-cef1-455f-a341-b15d8a5b3967",
+        },
+        body: null,
+      },
+      expectedStatusCode: 404,
+    },
+    {
+      name: "No character found for a non existing user id",
+      request: {
+        headers: dummyHeaders,
+        pathParameters: {
+          "character-id": fakeCharacterId,
+        },
+        body: null,
+      },
+      expectedStatusCode: 404,
+    },
+  ];
+
+  invalidTestCases.forEach((_case) => {
+    test(_case.name, async () => {
+      mockDynamoDBGetResponse(fakeDynamoDBCharacterResponse);
+
+      const result = await getCharacter(_case.request);
+
+      expect(result.statusCode).toBe(_case.expectedStatusCode);
+    });
+  });
+});
+
+describe("Valid requests", () => {
+  const validTestCases = [
+    {
+      name: "Successfully get an existing character",
+      request: {
+        headers: fakeHeaders,
+        pathParameters: {
+          "character-id": fakeCharacterId,
+        },
+        body: null,
+      },
+      expectedStatusCode: 200,
+    },
+  ];
+
+  validTestCases.forEach((_case) => {
+    test(_case.name, async () => {
+      mockDynamoDBGetResponse(fakeDynamoDBCharacterResponse);
+
+      const result = await getCharacter(_case.request);
+
+      expect(result.statusCode).toBe(_case.expectedStatusCode);
+
+      const parsedBody = JSON.parse(result.body);
+      expect(parsedBody.userId).toBe(fakeUserId);
+      expect(parsedBody.characterId).toBe(_case.request.pathParameters["character-id"]);
+    });
+  });
+});
