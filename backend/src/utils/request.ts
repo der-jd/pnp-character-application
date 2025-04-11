d3eb38a901780abce34bf6d0ea1c536e7d1bee91
der-jd <89513886+der-jd@users.noreply.github.com>
2025-04-11 21:18:02 +0200

    Renaming

--- backend/src/utils/request.ts
+++ backend/src/utils/request.ts
@@ -1,15 +1,15 @@
-type bodyType = Record<string, string | number | undefined> | null;
+type Body = Record<string, string | number | undefined> | null;
 
 // Abstraction and simplification of APIGatewayProxyEvent to allow testing with less dependencies
 export interface Request {
   headers: Record<string, string | undefined>;
   pathParameters: Record<string, string | undefined> | null;
   queryStringParameters: Record<string, string | undefined> | null;
-  body: bodyType;
+  body: Body;
 }
 
 // The parse is necessary for Lambda tests via the AWS console
-export function parseBody(body: string | bodyType): bodyType {
+export function parseBody(body: string | Body): Body {
   if (!body) {
     return null;
   } else if (typeof body === "string") {
