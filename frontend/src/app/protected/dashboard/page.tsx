4edd1d3449389acf985118add9b8f6d97e784196
Philipp Eich <philipp.eich1992@gmail.com>
2025-04-13 10:33:08 +0200

    test

--- frontend/src/app/protected/dashboard/page.tsx
+++ frontend/src/app/protected/dashboard/page.tsx
@@ -3,7 +3,7 @@
 function Dashboard() {
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-2xl font-bold mb-4">Welcome to your Dashboard</h1>
+      <h1 className="text-2xl font-bold mb-4">Welcome to your Dashboard TEST</h1>
       {/* Add your dashboard content here */}
     </div>
   );
