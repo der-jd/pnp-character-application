e3a44c6a34016a3ae2d979b8e4a514653038a7f5
der-jd <89513886+der-jd@users.noreply.github.com>
2025-04-14 22:20:57 +0200

    Add test cases for all schemas

--- backend/test/add-history-record/add-history-record.test.ts
+++ backend/test/add-history-record/add-history-record.test.ts
@@ -10,7 +10,7 @@ import {
 import { fakeCharacterId } from "../test-data/character.js";
 import { addRecordToHistory } from "add-history-record/index.js";
 import { fakeHistoryBlock2 } from "../test-data/history.js";
-import { RecordType } from "config/index.js";
+import { CostCategory, RecordType } from "config/index.js";
 
 const testBody = {
   type: RecordType.EVENT_CALCULATION_POINTS,
@@ -32,7 +32,6 @@ const testBody = {
     old: 100,
     new: 120,
   },
-  learningMethod: undefined,
   comment: "Epic fight against a big monster",
 };
 
@@ -151,14 +150,398 @@ describe("Invalid requests", () => {
 describe("Valid requests", () => {
   const validTestCases = [
     {
-      name: "Add history record for event 'calculation points' to existing block",
+      name: "Add history record for 'event calculation points' to existing block",
       request: {
         headers: fakeHeaders,
         pathParameters: {
           "character-id": fakeCharacterId,
         },
         queryStringParameters: null,
-        body: testBody,
+        body: {
+          type: RecordType.EVENT_CALCULATION_POINTS,
+          name: "Adventure Points",
+          data: {
+            old: {
+              start: 0,
+              available: 90,
+              total: 100,
+            },
+            new: {
+              start: 0,
+              available: 110,
+              total: 120,
+            },
+          },
+          learningMethod: undefined,
+          calculationPointsChange: {
+            adjustment: 20,
+            old: 90,
+            new: 110,
+          },
+          comment: "Epic fight against a big monster",
+        },
+      },
+      expectedStatusCode: 200,
+    },
+    {
+      name: "Add history record for 'event level up' to existing block",
+      request: {
+        headers: fakeHeaders,
+        pathParameters: {
+          "character-id": fakeCharacterId,
+        },
+        queryStringParameters: null,
+        body: {
+          type: RecordType.EVENT_LEVEL_UP,
+          name: "Level 2",
+          data: {
+            old: {
+              value: 1,
+            },
+            new: {
+              value: 2,
+            },
+          },
+          learningMethod: undefined,
+          calculationPointsChange: {
+            adjustment: 0,
+            old: 0,
+            new: 0,
+          },
+          comment: "Finished story arc",
+        },
+      },
+      expectedStatusCode: 200,
+    },
+    {
+      name: "Add history record for 'event base value' to existing block",
+      request: {
+        headers: fakeHeaders,
+        pathParameters: {
+          "character-id": fakeCharacterId,
+        },
+        queryStringParameters: null,
+        body: {
+          type: RecordType.EVENT_BASE_VALUE,
+          name: "health points",
+          data: {
+            old: {
+              start: 20,
+              current: 20,
+              byLvlUp: 0,
+              mod: 0,
+              totalCost: 0,
+            },
+            new: {
+              start: 20,
+              current: 26,
+              byLvlUp: 6,
+              mod: 0,
+              totalCost: 0,
+            },
+          },
+          learningMethod: undefined,
+          calculationPointsChange: {
+            adjustment: 0,
+            old: 110,
+            new: 110,
+          },
+          comment: "Level 2",
+        },
+      },
+      expectedStatusCode: 200,
+    },
+    {
+      name: "Add history record for 'profession changed' to existing block",
+      request: {
+        headers: fakeHeaders,
+        pathParameters: {
+          "character-id": fakeCharacterId,
+        },
+        queryStringParameters: null,
+        body: {
+          type: RecordType.PROFESSION_CHANGED,
+          name: "Profession changed",
+          data: {
+            old: {
+              name: "",
+              skill: "",
+            },
+            new: {
+              name: "Stonemason",
+              skill: "stonework",
+            },
+          },
+          learningMethod: undefined,
+          calculationPointsChange: {
+            adjustment: 0,
+            old: 0,
+            new: 0,
+          },
+          comment: undefined,
+        },
+      },
+      expectedStatusCode: 200,
+    },
+    {
+      name: "Add history record for 'hobby changed' to existing block",
+      request: {
+        headers: fakeHeaders,
+        pathParameters: {
+          "character-id": fakeCharacterId,
+        },
+        queryStringParameters: null,
+        body: {
+          type: RecordType.HOBBY_CHANGED,
+          name: "Hobby changed",
+          data: {
+            old: {
+              name: "",
+              skill: "",
+            },
+            new: {
+              name: "Musician",
+              skill: "makingMusic",
+            },
+          },
+          learningMethod: undefined,
+          calculationPointsChange: {
+            adjustment: 0,
+            old: 0,
+            new: 0,
+          },
+          comment: undefined,
+        },
+      },
+      expectedStatusCode: 200,
+    },
+    {
+      name: "Add history record for 'advantage changed' to existing block",
+      request: {
+        headers: fakeHeaders,
+        pathParameters: {
+          "character-id": fakeCharacterId,
+        },
+        queryStringParameters: null,
+        body: {
+          type: RecordType.ADVANTAGE_CHANGED,
+          name: "Advantage changed",
+          data: {
+            old: {
+              value: "",
+            },
+            new: {
+              value: "Iron Will",
+            },
+          },
+          learningMethod: undefined,
+          calculationPointsChange: {
+            adjustment: 0,
+            old: 110,
+            new: 110,
+          },
+          comment: undefined,
+        },
+      },
+      expectedStatusCode: 200,
+    },
+    {
+      name: "Add history record for 'disadvantage changed' to existing block",
+      request: {
+        headers: fakeHeaders,
+        pathParameters: {
+          "character-id": fakeCharacterId,
+        },
+        queryStringParameters: null,
+        body: {
+          type: RecordType.DISADVANTAGE_CHANGED,
+          name: "Disadvantage changed",
+          data: {
+            old: {
+              value: "",
+            },
+            new: {
+              value: "Short Temper",
+            },
+          },
+          learningMethod: undefined,
+          calculationPointsChange: {
+            adjustment: 0,
+            old: 110,
+            new: 110,
+          },
+          comment: undefined,
+        },
+      },
+      expectedStatusCode: 200,
+    },
+    {
+      name: "Add history record for 'special ability changed' to existing block",
+      request: {
+        headers: fakeHeaders,
+        pathParameters: {
+          "character-id": fakeCharacterId,
+        },
+        queryStringParameters: null,
+        body: {
+          type: RecordType.SPECIAL_ABILITY_CHANGED,
+          name: "Special ability changed",
+          data: {
+            old: {
+              value: "",
+            },
+            new: {
+              value: "Berserker Rage",
+            },
+          },
+          learningMethod: undefined,
+          calculationPointsChange: {
+            adjustment: 0,
+            old: 110,
+            new: 110,
+          },
+          comment: undefined,
+        },
+      },
+      expectedStatusCode: 200,
+    },
+    {
+      name: "Add history record for 'attribute raised' to existing block",
+      request: {
+        headers: fakeHeaders,
+        pathParameters: {
+          "character-id": fakeCharacterId,
+        },
+        queryStringParameters: null,
+        body: {
+          type: RecordType.ATTRIBUTE_RAISED,
+          name: "Courage",
+          data: {
+            old: {
+              start: 0,
+              current: 0,
+              mod: 0,
+              totalCost: 0,
+            },
+            new: {
+              start: 0,
+              current: 1,
+              mod: 0,
+              totalCost: 1,
+            },
+          },
+          learningMethod: undefined,
+          calculationPointsChange: {
+            adjustment: -1,
+            old: 9,
+            new: 8,
+          },
+          comment: undefined,
+        },
+      },
+      expectedStatusCode: 200,
+    },
+    {
+      name: "Add history record for 'skill activated' to existing block",
+      request: {
+        headers: fakeHeaders,
+        pathParameters: {
+          "character-id": fakeCharacterId,
+        },
+        queryStringParameters: null,
+        body: {
+          type: RecordType.SKILL_ACTIVATED,
+          name: "Disguising",
+          data: {
+            old: {
+              value: false,
+            },
+            new: {
+              value: true,
+            },
+          },
+          learningMethod: "NORMAL",
+          calculationPointsChange: {
+            adjustment: -50,
+            old: 110,
+            new: 60,
+          },
+          comment: undefined,
+        },
+      },
+      expectedStatusCode: 200,
+    },
+    {
+      name: "Add history record for 'skill raised' to existing block",
+      request: {
+        headers: fakeHeaders,
+        pathParameters: {
+          "character-id": fakeCharacterId,
+        },
+        queryStringParameters: null,
+        body: {
+          type: RecordType.SKILL_RAISED,
+          name: "Body Control",
+          data: {
+            old: {
+              activated: true,
+              start: 0,
+              current: 30,
+              mod: 0,
+              totalCost: 30,
+              defaultCostCategory: CostCategory.CAT_2,
+            },
+            new: {
+              activated: true,
+              start: 0,
+              current: 35,
+              mod: 0,
+              totalCost: 35,
+              defaultCostCategory: CostCategory.CAT_2,
+            },
+          },
+          learningMethod: "NORMAL",
+          calculationPointsChange: {
+            adjustment: -5,
+            old: 60,
+            new: 55,
+          },
+          comment: undefined,
+        },
+      },
+      expectedStatusCode: 200,
+    },
+    {
+      name: "Add history record for 'attack/parade distributed' to existing block",
+      request: {
+        headers: fakeHeaders,
+        pathParameters: {
+          "character-id": fakeCharacterId,
+        },
+        queryStringParameters: null,
+        body: {
+          type: RecordType.ATTACK_PARADE_DISTRIBUTED,
+          name: "Slashing Weapons 1h",
+          data: {
+            old: {
+              handling: 30,
+              attackDistributed: 10,
+              paradeDistributed: 10,
+            },
+            new: {
+              handling: 30,
+              attackDistributed: 16,
+              paradeDistributed: 12,
+            },
+          },
+          learningMethod: undefined,
+          calculationPointsChange: {
+            adjustment: 0,
+            old: 0,
+            new: 0,
+          },
+          comment: undefined,
+        },
       },
       expectedStatusCode: 200,
     },
