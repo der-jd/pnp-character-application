2210ceba9f5666516359db1b52db011aae9c3cf5
der-jd <89513886+der-jd@users.noreply.github.com>
2025-04-20 10:09:08 +0200

    Remove obsolete functionality for cors headers due to 'AWS' integration in API Gateway

--- backend/src/lambdas/create-tenant-id/index.ts
+++ backend/src/lambdas/create-tenant-id/index.ts
@@ -1,6 +1,5 @@
 import { CognitoIdentityProvider } from "@aws-sdk/client-cognito-identity-provider";
 import * as jwt from "jsonwebtoken";
-import { add_cors_headers, api_error } from "config/index.js";
 
 import { APIGatewayProxyEvent } from "aws-lambda";
 
@@ -17,16 +16,22 @@ async function refreshTokens(refreshToken: string, clientId: string) {
     });
 
     if (response.AuthenticationResult) {
-      return add_cors_headers({
+      return {
         statusCode: 200,
         body: JSON.stringify(response.AuthenticationResult),
-      });
+      };
     } else {
-      return api_error("Could not refresh token");
+      return {
+        statusCode: 500,
+        body: "Error: Could not refresh token",
+      };
     }
   } catch (error) {
     console.error(error);
-    return api_error("Could not refresh tokens!");
+    return {
+      statusCode: 500,
+      body: "Error: Could not refresh tokens!",
+    };
   }
 }
 
@@ -35,11 +40,17 @@ export const handler = async (event: APIGatewayProxyEvent) => {
   const clientId = process.env.CLIENT_ID;
 
   if (!userPoolId || !clientId) {
-    return api_error("User pool ID or client ID is not set in environment variables.");
+    return {
+      statusCode: 500,
+      body: "Error: User pool ID or client ID is not set in environment variables.",
+    };
   }
 
   if (!event.headers.Authorization) {
-    return api_error("No Authorization header received!");
+    return {
+      statusCode: 500,
+      body: "Error: No Authorization header received!",
+    };
   }
 
   const id_token = event.headers.Authorization.split(" ")[1];
@@ -51,17 +62,26 @@ export const handler = async (event: APIGatewayProxyEvent) => {
     const decoded: jwt.JwtPayload | string | null = jwt.decode(id_token);
 
     if (!decoded || typeof decoded == "string") {
-      return api_error("Invalid token!");
+      return {
+        statusCode: 500,
+        body: "Error: Invalid token!",
+      };
     }
 
     if (!decoded.sub) {
-      return api_error("Sub field not supplied!");
+      return {
+        statusCode: 500,
+        body: "Error: Sub field not supplied!",
+      };
     }
 
     sub = decoded.sub;
   } catch (error) {
     console.error(error);
-    return api_error("Invalid token!");
+    return {
+      statusCode: 500,
+      body: "Error: Invalid token!",
+    };
   }
 
   try {
@@ -78,30 +98,42 @@ export const handler = async (event: APIGatewayProxyEvent) => {
     });
   } catch (error) {
     if (error instanceof Error) {
-      return api_error("Processing request failed!");
+      return {
+        statusCode: 500,
+        body: "Error: Processing request failed!",
+      };
     } else {
-      return api_error("Unknown error occurred!");
+      return {
+        statusCode: 500,
+        body: "Error: Unknown error occurred!",
+      };
     }
   }
 
   if (!refresh_token) {
-    return api_error("Refresh token missing!");
+    return {
+      statusCode: 500,
+      body: "Error: Refresh token missing!",
+    };
   }
 
   try {
     const updated_tokens = await refreshTokens(refresh_token, clientId);
 
-    return add_cors_headers({
+    return {
       statusCode: 200,
       body: JSON.stringify({
         message: "Tokens refreshed successfully.",
         tokens: updated_tokens,
       }),
-    });
+    };
   } catch (error) {
     if (error instanceof Error) {
       console.error(error);
-      return api_error("Failed to refresh tokens!");
+      return {
+        statusCode: 500,
+        body: "Error: Failed to refresh tokens!",
+      };
     }
   }
 };
