fd73b852925ff81ad36c94ea4819120364aaa61e
der-jd <89513886+der-jd@users.noreply.github.com>
2025-05-23 19:15:45 +0200

    fix: Add missing dependency

--- terraform/modules/apigw_options_method/main.tf
+++ terraform/modules/apigw_options_method/main.tf
@@ -40,7 +40,7 @@ resource "aws_api_gateway_integration" "options" {
 }
 
 resource "aws_api_gateway_integration_response" "options" {
-  depends_on = [aws_api_gateway_integration.options]
+  depends_on = [aws_api_gateway_integration.options, aws_api_gateway_method_response.options]
 
   rest_api_id = var.rest_api_id
   resource_id = var.resource_id
