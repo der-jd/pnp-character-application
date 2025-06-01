2dd37b0b183877fe6f558695baecacee9e8408eb
Philipp Eich <philipp.eich1992@gmail.com>
2025-06-01 08:59:23 +0200

    connected short term history information to to character skill mode

--- frontend/src/app/protected/history/page.tsx
+++ frontend/src/app/protected/history/page.tsx
@@ -3,13 +3,13 @@
 import { columns } from "@/src/lib/components/ui/historyTable/columns";
 import { DataTable } from "@/src/lib/components/ui/historyTable/dataTable";
 import sampleData from "@api/models/history/historySample.json";
-// import { useCharacterStore } from "../../global/characterStore";
+import { useCharacterStore } from "../../global/characterStore";
 // import { useHistory } from "@/src/hooks/useHistory";
 // import { useEffect } from "react";
 // import { useLoadingOverlay } from "../../global/OverlayContext";
 
 export default function History() {
-  // const characterName = useCharacterStore((state) => state.characterSheet?.generalInformation.name);
+  const characterName = useCharacterStore((state) => state.characterSheet?.generalInformation.name);
   // const { updateHistory, isLoading } = useHistory();
   // const { show, hide } = useLoadingOverlay();
 
