005d7a95b84317603c87563a9e1b9c8dd9bdb3eb
der-jd <89513886+der-jd@users.noreply.github.com>
2025-06-19 13:00:18 +0200

    Log id in case of error

--- backend/src/utils/utils.ts
+++ backend/src/utils/utils.ts
@@ -7,6 +7,6 @@ export function validateUUID(id: string): void {
 
   const uuidRegex = new RegExp("^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$");
   if (!uuidRegex.test(id)) {
-    throw new HttpError(400, "Given id is not a valid UUID format!");
+    throw new HttpError(400, "Given id is not a valid UUID format!", { id: id });
   }
 }
