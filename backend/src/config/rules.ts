ea71b89faee4242e049f540e080503abdf8a1014
der-jd <89513886+der-jd@users.noreply.github.com>
2025-07-06 22:18:15 +0200

    Add code to create character (WIP)

--- backend/src/config/rules.ts
+++ backend/src/config/rules.ts
@@ -1,4 +1,50 @@
+import { combatSkills } from "./character.js";
+import { CostCategory } from "./cost.js";
+
+export const ATTRIBUTE_POINTS_FOR_CREATION = 40;
+
 export const MIN_ATTRIBUTE_VALUE_FOR_CREATION = 4;
 export const MAX_ATTRIBUTE_VALUE_FOR_CREATION = 7;
 
-export const MAX_ACTIVATABLE_SKILLS_FOR_CREATION = 5;
+export const PROFESSION_SKILL_BONUS = 50;
+export const HOBBY_SKILL_BONUS = 25;
+
+export const NUMBER_OF_ACTIVATABLE_SKILLS_FOR_CREATION = 5;
+
+export const START_LEVEL = 1;
+
+export const COST_CATEGORY_DEFAULT = CostCategory.CAT_2;
+export const COST_CATEGORY_COMBAT_SKILLS = CostCategory.CAT_3;
+
+export const startSkills: string[] = [
+  // body skills
+  "athletics",
+  "climbing",
+  "bodyControl",
+  "sneaking",
+  "swimming",
+  "selfControl",
+  "hiding",
+  "singing",
+  "sharpnessOfSenses",
+  "quaffing",
+  // social skills
+  "etiquette",
+  "knowledgeOfHumanNature",
+  "persuading",
+  // nature skills
+  "knottingSkills",
+  // knowledge skills
+  "mathematics",
+  "zoology",
+  // handcraft skills
+  "woodwork",
+  "foodProcessing",
+  "fabricProcessing",
+  "steeringVehicles",
+  "bargaining",
+  "firstAid",
+  "calmingSbDown",
+  "drawingAndPainting",
+  ...combatSkills,
+];
