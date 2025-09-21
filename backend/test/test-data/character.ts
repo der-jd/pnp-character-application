8aa3020ab902e6641708ec2c0253a9c90dd51c3e
der-jd <89513886+der-jd@users.noreply.github.com>
2025-09-21 13:19:45 -0400

    fix: remove byLvlUp field from naturalArmor base value (not allowed by ruleset)

--- backend/test/test-data/character.ts
+++ backend/test/test-data/character.ts
@@ -123,7 +123,6 @@ export const fakeCharacter: Character = {
       naturalArmor: {
         start: 0,
         current: 10,
-        byLvlUp: 10,
         mod: 5,
       },
       initiativeBaseValue: {
@@ -1060,7 +1059,6 @@ export const fakeCharacter2: Character = {
       naturalArmor: {
         start: 0,
         current: 10,
-        byLvlUp: 10,
         mod: 5,
       },
       initiativeBaseValue: {
