a2f6d51aa8dda66cdefc6cda684e8551737a2835
der-jd <89513886+der-jd@users.noreply.github.com>
2025-09-20 22:59:47 -0400

    feat: add ESLint configuration and dependencies to api-spec package

---
+++ api-spec/eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+  {
+    ignores: ["**/node_modules/", "dist/"],
+  },
+  {
+    languageOptions: { globals: globals.node },
+  },
+];
