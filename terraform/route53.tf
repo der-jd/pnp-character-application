403f89f506d34c499ca1cd54c35ed53053d0569c
der-jd <89513886+der-jd@users.noreply.github.com>
2026-02-14 16:35:37 -0500

    Change www subdomain DNS record from A alias to CNAME pointing to root domain

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -54,11 +54,7 @@ resource "aws_route53_record" "frontend" {
 resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "www.${var.domain_name}"
-  type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.frontend_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.frontend_distribution.hosted_zone_id
-    evaluate_target_health = false
-  }
+  type    = "CNAME"
+  ttl     = 300
+  records = [var.domain_name]
 }
