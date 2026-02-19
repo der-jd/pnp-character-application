15974bdd223a471c38a7a214fbda693e7a08f88e
jd-tmh <121879641+jd-tmh@users.noreply.github.com>
2026-02-18 22:00:32 -0500

    Organize clone-character tests into describe blocks for invalid requests and valid requests

--- backend/test/component-tests/clone-character/clone-character.test.ts
+++ backend/test/component-tests/clone-character/clone-character.test.ts
@@ -14,100 +14,116 @@ describe("clone-character component tests", () => {
     await cleanUpTestContext();
   });
 
-  test("authorization header is missing", async () => {
-    // Create a client without authorization header
-    const unauthorizedClient = new ApiClient(getTestContext().apiBaseUrl, "");
+  /**
+   * =============================
+   * Invalid requests
+   * =============================
+   */
 
-    await expectApiError(
-      () =>
-        unauthorizedClient.post(`characters/${getTestContext().character.characterId}/clone`, {
+  describe("Invalid requests", () => {
+    test("authorization header is missing", async () => {
+      // Create a client without authorization header
+      const unauthorizedClient = new ApiClient(getTestContext().apiBaseUrl, "");
+
+      await expectApiError(
+        () =>
+          unauthorizedClient.post(`characters/${getTestContext().character.characterId}/clone`, {
+            userIdOfCharacter: getTestContext().character.userId,
+          }),
+        401,
+        "Unauthorized",
+      );
+    });
+
+    test("authorization token is invalid", async () => {
+      // Create a client with invalid authorization
+      const malformedClient = new ApiClient(getTestContext().apiBaseUrl, "Bearer 1234567890");
+
+      await expectApiError(
+        () =>
+          malformedClient.post(`characters/${getTestContext().character.characterId}/clone`, {
+            userIdOfCharacter: getTestContext().character.userId,
+          }),
+        401,
+        "Unauthorized",
+      );
+    });
+
+    test("character id is not a uuid", async () => {
+      await expectApiError(
+        () =>
+          apiClient.post(`characters/${INVALID_UUID}/clone`, {
+            userIdOfCharacter: getTestContext().character.userId,
+          }),
+        400,
+        "Invalid input values",
+      );
+    });
+
+    test("no character found for non-existing character id", async () => {
+      await expectApiError(
+        () =>
+          apiClient.post(`characters/${NON_EXISTENT_UUID}/clone`, {
+            userIdOfCharacter: getTestContext().character.userId,
+          }),
+        404,
+        "No character found",
+      );
+    });
+
+    test("no character found for non-existing user id", async () => {
+      await expectApiError(
+        () =>
+          apiClient.post(`characters/${getTestContext().character.characterId}/clone`, {
+            userIdOfCharacter: NON_EXISTENT_UUID,
+          }),
+        404,
+        "No character found",
+      );
+    });
+  });
+
+  /**
+   * =============================
+   * Valid requests
+   * =============================
+   */
+
+  describe("Valid requests", () => {
+    test("successfully clone an own character", async () => {
+      const cloneResponse = postCharacterCloneResponseSchema.parse(
+        await apiClient.post(`characters/${getTestContext().character.characterId}/clone`, {
           userIdOfCharacter: getTestContext().character.userId,
         }),
-      401,
-      "Unauthorized",
-    );
-  });
+      );
 
-  test("authorization token is invalid", async () => {
-    // Create a client with invalid authorization
-    const malformedClient = new ApiClient(getTestContext().apiBaseUrl, "Bearer 1234567890");
+      try {
+        expect(cloneResponse.characterId).not.toBe(getTestContext().character.characterId);
+        expect(cloneResponse.userId).toBe(getTestContext().character.userId);
+        expect(cloneResponse.name).not.toBe(getTestContext().character.characterSheet.generalInformation.name);
+        expect(cloneResponse.name).toContain("Copy");
+        expect(cloneResponse.level).toBe(getTestContext().character.characterSheet.generalInformation.level);
 
-    await expectApiError(
-      () =>
-        malformedClient.post(`characters/${getTestContext().character.characterId}/clone`, {
-          userIdOfCharacter: getTestContext().character.userId,
-        }),
-      401,
-      "Unauthorized",
-    );
-  });
+        const cloned = getCharacterResponseSchema.parse(await apiClient.get(`characters/${cloneResponse.characterId}`));
+        expect(cloned.characterId).toBe(cloneResponse.characterId);
+        expect(cloned.characterSheet.generalInformation.name).toBe(cloneResponse.name);
 
-  test("character id is not a uuid", async () => {
-    await expectApiError(
-      () =>
-        apiClient.post(`characters/${INVALID_UUID}/clone`, {
-          userIdOfCharacter: getTestContext().character.userId,
-        }),
-      400,
-      "Invalid input values",
-    );
-  });
-
-  test("no character found for non-existing character id", async () => {
-    await expectApiError(
-      () =>
-        apiClient.post(`characters/${NON_EXISTENT_UUID}/clone`, {
-          userIdOfCharacter: getTestContext().character.userId,
-        }),
-      404,
-      "No character found",
-    );
-  });
-
-  test("no character found for non-existing user id", async () => {
-    await expectApiError(
-      () =>
-        apiClient.post(`characters/${getTestContext().character.characterId}/clone`, {
-          userIdOfCharacter: NON_EXISTENT_UUID,
-        }),
-      404,
-      "No character found",
-    );
-  });
-
-  test("successfully clone an own character", async () => {
-    const cloneResponse = postCharacterCloneResponseSchema.parse(
-      await apiClient.post(`characters/${getTestContext().character.characterId}/clone`, {
-        userIdOfCharacter: getTestContext().character.userId,
-      }),
-    );
-
-    try {
-      expect(cloneResponse.characterId).not.toBe(getTestContext().character.characterId);
-      expect(cloneResponse.userId).toBe(getTestContext().character.userId);
-      expect(cloneResponse.name).not.toBe(getTestContext().character.characterSheet.generalInformation.name);
-      expect(cloneResponse.name).toContain("Copy");
-      expect(cloneResponse.level).toBe(getTestContext().character.characterSheet.generalInformation.level);
-
-      const cloned = getCharacterResponseSchema.parse(await apiClient.get(`characters/${cloneResponse.characterId}`));
-      expect(cloned.characterId).toBe(cloneResponse.characterId);
-      expect(cloned.characterSheet.generalInformation.name).toBe(cloneResponse.name);
-
-      // Verify the cloned character is identical to the original except for the character id and name
-      const expectedSheet = {
-        userId: getTestContext().character.userId,
-        characterId: cloneResponse.characterId,
-        characterSheet: {
-          ...getTestContext().character.characterSheet,
-          generalInformation: {
-            ...getTestContext().character.characterSheet.generalInformation,
-            name: cloneResponse.name,
+        // Verify the cloned character is identical to the original except for the character id and name
+        const expectedSheet = {
+          userId: getTestContext().character.userId,
+          characterId: cloneResponse.characterId,
+          characterSheet: {
+            ...getTestContext().character.characterSheet,
+            generalInformation: {
+              ...getTestContext().character.characterSheet.generalInformation,
+              name: cloneResponse.name,
+            },
           },
-        },
-      };
-      expect(cloned.characterSheet).toStrictEqual(expectedSheet);
-    } finally {
-      await deleteCharacter(cloneResponse.characterId);
-    }
+        };
+        expect(cloned.characterSheet).toStrictEqual(expectedSheet);
+      } finally {
+        await deleteCharacter(cloneResponse.characterId);
+      }
+    });
   });
 });
