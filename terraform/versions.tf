0f9dc3652e06c89dc88b957e95c8feb45b15cc4a
jd-tmh <121879641+jd-tmh@users.noreply.github.com>
2026-02-20 00:12:06 -0500

    Relax Terraform provider version constraints to accept patch and minor updates

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,19 +2,19 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.33"
+      version = "~> 6"
     }
 
     archive = {
       source  = "hashicorp/archive"
-      version = "~> 2.7"
+      version = "~> 2"
     }
 
     local = {
       source  = "hashicorp/local"
-      version = "~> 2.7"
+      version = "~> 2"
     }
   }
 
-  required_version = ">= 1.14.5"
+  required_version = "~> 1"
 }
