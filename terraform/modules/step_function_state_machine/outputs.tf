83ef140a4b86cd623282d635f063871703eb55bb
der-jd <89513886+der-jd@users.noreply.github.com>
2026-02-28 21:35:08 -0500

    Remove unused state_machine_name output from step function module

--- terraform/modules/step_function_state_machine/outputs.tf
+++ terraform/modules/step_function_state_machine/outputs.tf
@@ -2,8 +2,3 @@ output "state_machine_arn" {
   description = "ARN of the step function state machine"
   value       = aws_sfn_state_machine.state_machine.arn
 }
-
-output "state_machine_name" {
-  description = "Name of the step function state machine"
-  value       = aws_sfn_state_machine.state_machine.name
-}
