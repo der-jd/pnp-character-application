cfa933902675196a389bbdc57b70b61bc881e728
der-jd <89513886+der-jd@users.noreply.github.com>
2026-03-01 21:27:12 -0500

    Update RequestBody type definition and parseBody function to handle null values correctly

--- backend/src/core/request.ts
+++ backend/src/core/request.ts
@@ -1,15 +1,15 @@
-export type RequestBody = Record<string, any> | null;
+export type RequestBody = Record<string, any>;
 
 // Abstraction and simplification of APIGatewayProxyEvent to allow testing with less dependencies
 export interface Request {
   headers: Record<string, string | undefined>;
   pathParameters: Record<string, string | undefined> | null;
   queryStringParameters: Record<string, string | undefined> | null;
-  body: RequestBody;
+  body: RequestBody | null;
 }
 
 // The parse is necessary for Lambda tests via the AWS console
-export function parseBody(body: string | RequestBody): RequestBody {
+export function parseBody(body: string | RequestBody | null): RequestBody | null {
   if (!body) {
     return null;
   } else if (typeof body === "string") {
